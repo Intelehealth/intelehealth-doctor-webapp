4abfaaa3d7dfb958c5c52aabc16d5d124b37916e
kundanmail55 <kundanmail55@gmail.com>
2019-06-24 19:37:12 +0530

    doctor profile page added

--- src/app/component/my-account/my-account.component.ts
+++ src/app/component/my-account/my-account.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { EncounterService } from 'src/app/services/encounter.service';
 import { HttpClient } from '@angular/common/http';
-import { FormGroup, FormControl } from '@angular/forms';
+import { MatDialog } from '@angular/material';
+import { SignatureComponent } from './signature/signature.component';
 
 @Component({
   selector: 'app-my-account',
@@ -10,22 +11,20 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class MyAccountComponent implements OnInit {
   baseUrl = window.location.host;
-  status = false;
   name = 'Enter text';
   providerDetails = null;
 
-  addSignatureForm = new FormGroup({
-    signature: new FormControl(''),
-    text: new FormControl('')
-  });
+
 
   constructor(private service: EncounterService,
-              private http: HttpClient) { }
+              private http: HttpClient,
+              private dialog : MatDialog) { }
 
   ngOnInit() {
     this.service.session()
       .subscribe(session => {
         const userUuid = session.user.uuid;
+        console.log(userUuid);
         this.service.provider(userUuid)
           .subscribe(provider => {
             this.providerDetails = provider.results[0];
@@ -37,53 +36,8 @@ export class MyAccountComponent implements OnInit {
       });
   }
 
-  onSubmit() {
-    const formValue = this.addSignatureForm.value;
-    const signatureValue = formValue.signature;
-    const signText = formValue.text;
-    if (signatureValue === '1') {
-      signature(signText, 'Arty');
-    }
-    if (signatureValue === '2') {
-      signature(signText, 'Asem');
-    }
-    if (signatureValue === '3') {
-      signature(signText, 'Youthness');
-    }
+  signature() {
+    this.dialog.open(SignatureComponent, { width: '500px' });
   }
+
 }
-
-function signature(text: string, font: string) {
-  this.service.session()
-    .subscribe(response => {
-      const userUuid = response.user.uuid;
-      const url = `http://${this.baseUrl}/openmrs/ws/rest/v1/provider?user=${userUuid}`;
-      this.http.get(url)
-        .subscribe(response1 => {
-          const providerUuid = response1.results[0].uuid;
-          const url1 = `http://${this.baseUrl}/openmrs/ws/rest/v1/provider/${providerUuid}/attribute`;
-          this.http.get(url1)
-            .subscribe(response2 => {
-              const data = response2;
-              if (data.results.length !== 0) {
-                this.status = true;
-              } else {
-                const url2 = `http://${this.baseUrl}/openmrs/ws/rest/v1/provider/${providerUuid}/attribute`;
-                const json = {
-                  'attributetype': '',
-                  'value': text
-                };
-                this.http.post(url2, json);
-                const url3 = `http://${this.baseUrl}/openmrs/ws/rest/v1/provider/${providerUuid}/attribute`;
-                const json1 = {
-                  'attributetype': '',
-                  'value': font
-                };
-                this.http.post(url3, json1);
-              }
-            });
-        });
-
-    });
-}
-
---
+++ src/app/component/my-account/signature/signature.component.ts
@@ -0,0 +1,83 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { EncounterService } from 'src/app/services/encounter.service';
+import { MatDialogRef } from '@angular/material';
+
+
+@Component({
+  selector: 'app-signature',
+  templateUrl: './signature.component.html',
+  styleUrls: ['./signature.component.css']
+})
+export class SignatureComponent implements OnInit {
+  addSignatureForm = new FormGroup({
+    signature: new FormControl(''),
+    text: new FormControl('')
+  });
+
+  baseUrl = window.location.host;
+  status = false;
+  name = 'Enter text';
+
+  constructor(private service: EncounterService,
+              private http: HttpClient,
+              private dialogRef: MatDialogRef<SignatureComponent>) { }
+
+  ngOnInit() {
+  }
+
+  onClose() {
+    this.dialogRef.close();
+  }
+
+
+onSubmit() {
+  const formValue = this.addSignatureForm.value;
+  const signatureValue = formValue.signature;
+  const signText = formValue.text;
+  if (signatureValue === '1') {
+    signature(signText, 'Arty');
+  }
+  if (signatureValue === '2') {
+    signature(signText, 'Asem');
+  }
+  if (signatureValue === '3') {
+    signature(signText, 'Youthness');
+  }
+}
+}
+
+function signature(text: string, font: string) {
+this.service.session()
+  .subscribe(response => {
+    const userUuid = response.user.uuid;
+    const url = `http://${this.baseUrl}/openmrs/ws/rest/v1/provider?user=${userUuid}`;
+    this.http.get(url)
+      .subscribe(response1 => {
+        const providerUuid = response1.results[0].uuid;
+        const url1 = `http://${this.baseUrl}/openmrs/ws/rest/v1/provider/${providerUuid}/attribute`;
+        this.http.get(url1)
+          .subscribe(response2 => {
+            const data = response2;
+            if (data.results.length !== 0) {
+              this.status = true;
+            } else {
+              const url2 = `http://${this.baseUrl}/openmrs/ws/rest/v1/provider/${providerUuid}/attribute`;
+              const json = {
+                'attributetype': '',
+                'value': text
+              };
+              this.http.post(url2, json);
+              const url3 = `http://${this.baseUrl}/openmrs/ws/rest/v1/provider/${providerUuid}/attribute`;
+              const json1 = {
+                'attributetype': '',
+                'value': font
+              };
+              this.http.post(url3, json1);
+            }
+          });
+      });
+  });
+}
+
