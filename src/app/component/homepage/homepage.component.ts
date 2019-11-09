10427e5f73a83027a059639c23ce6a138d5ec577
kundanmail55 <kundanmail55@gmail.com>
2019-11-10 05:23:18 +0530

    reusable table component

--- src/app/component/homepage/homepage.component.ts
+++ src/app/component/homepage/homepage.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { VisitService } from 'src/app/services/visit.service';
-import { MatSnackBar, MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import { MatSnackBar } from '@angular/material';
 
 export interface VisitData {
   id: string;
@@ -15,6 +15,7 @@ export interface VisitData {
   provider: string;
 }
 
+
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
@@ -22,11 +23,6 @@ export interface VisitData {
 })
 
 export class HomepageComponent implements OnInit {
-  displayColumns: string[] = ['id', 'name', 'gender', 'dob', 'location', 'status', 'provider', 'lastSeen'];
-  dataSourceFlag;
-  dataSourceWaiting;
-  dataSourceProgress;
-  dataSourceComplete;
   value: any = {};
   activePatient: number;
   flagPatientNo: number;
@@ -35,21 +31,10 @@ export class HomepageComponent implements OnInit {
   waitingVisit: VisitData[] = [];
   progressVisit: VisitData[] = [];
   completedVisit: VisitData[] = [];
-  visitLength = 0;
-  flagLength = 0;
   setSpiner = true;
 
   constructor(private service: VisitService,
               private snackbar: MatSnackBar) { }
-
-  @ViewChild('page1') page1: MatPaginator;
-  @ViewChild('page2') page2: MatPaginator;
-  @ViewChild('page3') page3: MatPaginator;
-  @ViewChild('page4') page4: MatPaginator;
-  @ViewChild('sortCol1') sortCol1: MatSort;
-  @ViewChild('sortCol2') sortCol2: MatSort;
-  @ViewChild('sortCol3') sortCol3: MatSort;
-  @ViewChild('sortCol4') sortCol4: MatSort;
 
   ngOnInit() {
     this.service.getVisits()
@@ -64,7 +49,6 @@ export class HomepageComponent implements OnInit {
             if (value.match('Flagged')) {
               if (!active.encounters[0].voided) {
                 const values = this.assignValueToProperty(active);
-                console.log(values)
                 this.flagVisit.push(values);
                 flagLength += 1;
               }
@@ -75,31 +59,15 @@ export class HomepageComponent implements OnInit {
             } else if (value.match('Visit Note')) {
               const values = this.assignValueToProperty(active);
               this.progressVisit.push(values);
+              visitNoteLength += 1;
             } else if (value.match('Visit Complete')) {
               const values = this.assignValueToProperty(active);
               this.completedVisit.push(values);
-              visitNoteLength += 1;
             }
           }
           this.value = {};
         });
-        this.dataSourceFlag = new MatTableDataSource<VisitData>(this.flagVisit);
-        this.dataSourceWaiting = new MatTableDataSource<VisitData>(this.waitingVisit);
-        this.dataSourceProgress = new MatTableDataSource<VisitData>(this.progressVisit);
-        this.dataSourceComplete = new MatTableDataSource<VisitData>(this.completedVisit);
-        setTimeout(() => {
-          this.dataSourceFlag.paginator = this.page1;
-          this.dataSourceWaiting.paginator = this.page2;
-          this.dataSourceProgress.paginator = this.page3;
-          this.dataSourceComplete.paginator = this.page4;
-          this.dataSourceFlag.sort = this.sortCol1;
-          this.dataSourceWaiting.sort = this.sortCol2;
-          this.dataSourceProgress.sort = this.sortCol3;
-          this.dataSourceComplete.sort = this.sortCol4;
-          this.setSpiner = false;
-        }, 1000);
-        this.visitLength = this.waitingVisit.length;
-        this.flagLength = this.flagVisit.length;
+        this.setSpiner = false;
         this.activePatient = length;
         this.flagPatientNo = flagLength;
         this.visitNoteNo = visitNoteLength;
@@ -126,31 +94,4 @@ export class HomepageComponent implements OnInit {
     return this.value;
   }
 
-  applyFilterFlagVisit(filterValue: string) {
-    this.dataSourceFlag.filter = filterValue.trim().toLowerCase();
-    if (this.dataSourceFlag.paginator) {
-      this.dataSourceFlag.paginator.firstPage();
-    }
-  }
-
-  applyFilterWaitingVisit(filterValue: string) {
-    this.dataSourceWaiting.filter = filterValue.trim().toLowerCase();
-    if (this.dataSourceWaiting.paginator) {
-      this.dataSourceWaiting.paginator.firstPage();
-    }
-  }
-
-  applyFilterProgressVisit(filterValue: string) {
-    this.dataSourceProgress.filter = filterValue.trim().toLowerCase();
-    if (this.dataSourceProgress.paginator) {
-      this.dataSourceProgress.paginator.firstPage();
-    }
-  }
-
-  applyFilterCompleteVisit(filterValue: string) {
-    this.dataSourceComplete.filter = filterValue.trim().toLowerCase();
-    if (this.dataSourceComplete.paginator) {
-      this.dataSourceComplete.paginator.firstPage();
-    }
-  }
 }
