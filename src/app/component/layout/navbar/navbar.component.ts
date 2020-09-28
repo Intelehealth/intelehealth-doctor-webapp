24632f7a9d621bbd7b8ca03a86ca4862f62e0d00
shubham.jo <shubham@intelehealth.io>
2020-09-28 21:02:48 +0530

    - Added modal for notification

--- src/app/component/layout/navbar/navbar.component.ts
+++ src/app/component/layout/navbar/navbar.component.ts
@@ -1,20 +1,20 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { AuthService } from 'src/app/services/auth.service';
-import { MatDialog } from '@angular/material/dialog';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { ChangePasswordComponent } from '../../change-password/change-password.component';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
-import { FindPatientComponent } from '../../find-patient/find-patient.component';
-import { environment } from '../../../../environments/environment';
-import { SwPush, SwUpdate } from '@angular/service-worker';
-import { PushNotificationsService } from 'src/app/services/push-notification.service';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { AuthService } from "src/app/services/auth.service";
+import { MatDialog } from "@angular/material/dialog";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { ChangePasswordComponent } from "../../change-password/change-password.component";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { HttpClient } from "@angular/common/http";
+import { FindPatientComponent } from "../../find-patient/find-patient.component";
+import { environment } from "../../../../environments/environment";
+import { SwPush, SwUpdate } from "@angular/service-worker";
+import { PushNotificationsService } from "src/app/services/push-notification.service";
 declare var getFromStorage: any, saveToStorage: any;
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.css"],
 })
 export class NavbarComponent implements OnInit {
   baseURL = environment.baseURL;
@@ -24,38 +24,55 @@ export class NavbarComponent implements OnInit {
   subscribeAccess = false;
   notificationMenu = false;
   showBellIcon = false;
-  selectedNotification = '';
+  selectedNotification = "";
   values: any = [];
-  readonly VapidKEY = 'BFwuhYcJpWKFnTewNm9XtBTycAV_qvBqvIfbALC02CtOaMeXwrO6Zhm7MI_NIjDV9_TCbrr0FMmaDnZ7jllV6Xg';
+  weekDays: any = [
+    { day: "Monday", startTime: null, endTime: null },
+    { day: "Tuesday", startTime: null, endTime: null },
+    { day: "Wednesday", startTime: null, endTime: null },
+    { day: "Thursday", startTime: null, endTime: null },
+    { day: "Friday", startTime: null, endTime: null },
+    { day: "Saturday", startTime: null, endTime: null },
+    { day: "Sunday", startTime: null, endTime: null },
+  ];
+  readonly VapidKEY =
+    "BFwuhYcJpWKFnTewNm9XtBTycAV_qvBqvIfbALC02CtOaMeXwrO6Zhm7MI_NIjDV9_TCbrr0FMmaDnZ7jllV6Xg";
 
   searchForm = new FormGroup({
-    findInput: new FormControl('', [Validators.required])
+    findInput: new FormControl("", [Validators.required]),
   });
 
   @Output() messageEvent = new EventEmitter<string>();
 
-
-  constructor(private authService: AuthService,
-              private dialog: MatDialog,
-              private snackbar: MatSnackBar,
-              private http: HttpClient,
-              public swUpdate: SwUpdate,
-              public swPush: SwPush,
-              public notificationService: PushNotificationsService) { }
+  constructor(
+    private authService: AuthService,
+    private dialog: MatDialog,
+    private snackbar: MatSnackBar,
+    private http: HttpClient,
+    public swUpdate: SwUpdate,
+    public swPush: SwPush,
+    public notificationService: PushNotificationsService
+  ) {}
 
   ngOnInit() {
-    const userDetails = getFromStorage('user');
-    this.subscribeAccess = getFromStorage('subscribed') || false;
+    const userDetails = getFromStorage("user");
+    this.subscribeAccess = getFromStorage("subscribed") || false;
     if (userDetails) {
-      const roles = userDetails['roles'];
-      roles.forEach(role => {
-        if (role.uuid === 'f6de773b-277e-4ce2-9ee6-8622b8a293e8') {
+      const roles = userDetails["roles"];
+      roles.forEach((role) => {
+        if (role.uuid === "f6de773b-277e-4ce2-9ee6-8622b8a293e8") {
           this.systemAccess = true;
-        } if (role.uuid === 'f6de773b-277e-4ce2-9ee6-8622b8a293e8' || role.uuid === 'a5df6aa5-d6e5-4b56-b0e7-315ee0899f97') {
+        }
+        if (
+          role.uuid === "f6de773b-277e-4ce2-9ee6-8622b8a293e8" ||
+          role.uuid === "a5df6aa5-d6e5-4b56-b0e7-315ee0899f97"
+        ) {
           this.reportAccess = true;
         }
       });
-    } else { this.logout(); }
+    } else {
+      this.logout();
+    }
   }
 
   logout() {
@@ -63,34 +80,42 @@ export class NavbarComponent implements OnInit {
   }
 
   changePassword() {
-    this.dialog.open(ChangePasswordComponent, { width: '500px' });
+    this.dialog.open(ChangePasswordComponent, { width: "500px" });
   }
 
   search() {
     const search = this.searchForm.value;
     if (search.findInput.length < 3) {
-    this.dialog.open(FindPatientComponent, { width: '50%', data: { value: 'Please Enter min 3 characters' } });
+      this.dialog.open(FindPatientComponent, {
+        width: "50%",
+        data: { value: "Please Enter min 3 characters" },
+      });
     } else {
-    // tslint:disable-next-line: max-line-length
-    const url = `${this.baseURL}/patient?q=${search.findInput}&v=custom:(uuid,identifiers:(identifierType:(name),identifier),person)`;
-    this.http.get(url)
-    .subscribe(response => {
-      this.values = [];
-      response['results'].forEach(value => {
-        if (value) {
-        if (value.identifiers.length) {
-          this.values.push(value);
+      // tslint:disable-next-line: max-line-length
+      const url = `${this.baseURL}/patient?q=${search.findInput}&v=custom:(uuid,identifiers:(identifierType:(name),identifier),person)`;
+      this.http.get(url).subscribe(
+        (response) => {
+          this.values = [];
+          response["results"].forEach((value) => {
+            if (value) {
+              if (value.identifiers.length) {
+                this.values.push(value);
+              }
+            }
+          });
+          this.dialog.open(FindPatientComponent, {
+            width: "90%",
+            data: { value: this.values },
+          });
+        },
+        (err) => {
+          if (err.error instanceof Error) {
+            this.snackbar.open("Client-side error", null, { duration: 2000 });
+          } else {
+            this.snackbar.open("Server-side error", null, { duration: 2000 });
+          }
         }
-      }
-      });
-      this.dialog.open(FindPatientComponent, { width: '90%', data: { value: this.values } });
-    }, err => {
-        if (err.error instanceof Error) {
-          this.snackbar.open('Client-side error', null, { duration: 2000 });
-        } else {
-          this.snackbar.open('Server-side error', null, { duration: 2000 });
-        }
-      });
+      );
     }
     this.searchForm.reset();
   }
@@ -101,25 +126,41 @@ export class NavbarComponent implements OnInit {
 
   subscribeNotification() {
     if (this.swUpdate.isEnabled) {
-      this.swPush.requestSubscription({
-        serverPublicKey: this.VapidKEY
-      }).then(sub => {
-        const providerDetails = getFromStorage('provider');
-        if (providerDetails) {
-          const attributes = providerDetails.attributes;
-          attributes.forEach(element => {
-            if (element.attributeType.uuid === 'ed1715f5-93e2-404e-b3c9-2a2d9600f062' && !element.voided) {
-              this.notificationService.postSubscription(sub, element.value, providerDetails.person.display).subscribe(response => {
-                if (response) {
-                  this.snackbar.open(`Notification Subscribed Successfully`, null, {duration: 4000});
-                  saveToStorage('subscribed', true);
-                  this.subscribeAccess = true;
-                }
-              });
-            }
-          });
-        }
-      });
+      this.swPush
+        .requestSubscription({
+          serverPublicKey: this.VapidKEY,
+        })
+        .then((sub) => {
+          const providerDetails = getFromStorage("provider");
+          if (providerDetails) {
+            const attributes = providerDetails.attributes;
+            attributes.forEach((element) => {
+              if (
+                element.attributeType.uuid ===
+                  "ed1715f5-93e2-404e-b3c9-2a2d9600f062" &&
+                !element.voided
+              ) {
+                this.notificationService
+                  .postSubscription(
+                    sub,
+                    element.value,
+                    providerDetails.person.display
+                  )
+                  .subscribe((response) => {
+                    if (response) {
+                      this.snackbar.open(
+                        `Notification Subscribed Successfully`,
+                        null,
+                        { duration: 4000 }
+                      );
+                      saveToStorage("subscribed", true);
+                      this.subscribeAccess = true;
+                    }
+                  });
+              }
+            });
+          }
+        });
     }
   }
 
@@ -132,8 +173,10 @@ export class NavbarComponent implements OnInit {
     }
   }
 
-  setNotification(period){
-    this.selectedNotification = period;
+  setNotification(period) {
+    if (period !== "custom") {
+      this.selectedNotification = period;
+    }
     this.notificationMenu = false;
   }
 }
