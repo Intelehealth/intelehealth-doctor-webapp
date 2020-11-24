f67645f67a369cc547e7f49dfe7f9affe3032187
shubham.jo <shubham@intelehealth.io>
2020-11-24 12:55:11 +0530

    Add age functionality

--- src/app/component/visit-summary/patientinfo/patientinfo.component.ts
+++ src/app/component/visit-summary/patientinfo/patientinfo.component.ts
@@ -3,12 +3,14 @@ import { ImagesService } from 'src/app/services/images.service';
 import { ActivatedRoute } from '@angular/router';
 import { VisitService } from 'src/app/services/visit.service';
 import { environment } from '../../../../environments/environment';
-
+import { DatePipe } from '@angular/common';
+import * as moment from 'moment/moment';
 
 @Component({
   selector: 'app-patientinfo',
   templateUrl: './patientinfo.component.html',
-  styleUrls: ['./patientinfo.component.css']
+  styleUrls: ['./patientinfo.component.css'],
+  providers: [DatePipe]
 })
 
 export class PatientinfoComponent implements OnInit {
@@ -18,10 +20,13 @@ patientInfo = [];
 patientIdentifier: string;
 info = {};
 profileImagePresent = false;
+now: any
 
 constructor(private route: ActivatedRoute,
             private visitService: VisitService,
-            private service: ImagesService) { }
+            private service: ImagesService,
+            private datePipe: DatePipe,
+           ) { }
 
   ngOnInit() {
       const uuid = this.route.snapshot.paramMap.get('patient_id');
@@ -44,4 +49,22 @@ constructor(private route: ActivatedRoute,
         this.patientInfo.push(this.info);
       });
     }
+
+    getAge(dateString) {
+      var mydate = dateString.replace(
+        /^(\d{2})\/(\d{2})\/(\d{4})$/,
+        "$3, $2, $1"
+      );
+  
+      this.now = new Date();
+      var todayDate = this.datePipe.transform(this.now, "yyyy, MM, dd");
+  
+      var a = moment([todayDate]);
+      var b = moment([mydate]);
+  
+      var diffDuration = moment.duration(a.diff(b));
+      var ageString = diffDuration.years() + " years - " + diffDuration.months() + " months - " + diffDuration.days() + " days";
+      return ageString;
+  }
+
 }
