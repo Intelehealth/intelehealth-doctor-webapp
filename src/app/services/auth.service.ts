2308ffa620d63cfe39be323a34d3376986bf4424
Zeeshan <zeeshan@intelehealth.org>
2021-01-06 12:42:56 +0530

    - Resubscribing notifications to fix webpush error - Updated to get visitor fingerprint

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,25 +1,27 @@
-import { SessionService } from './session.service';
-import { Injectable } from '@angular/core';
-import { CookieService } from 'ngx-cookie-service';
-import { Router } from '@angular/router';
-
-declare var deleteFromStorage: any;
+import { SessionService } from "./session.service";
+import { Injectable } from "@angular/core";
+import { CookieService } from "ngx-cookie-service";
+import { Router } from "@angular/router";
+import FingerprintJS from "@fingerprintjs/fingerprintjs";
+declare var getFromStorage: any, saveToStorage: any, deleteFromStorage: any;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthService {
-
-  constructor(private myRoute: Router,
-              private sessionService: SessionService,
-              private cookieService: CookieService) { }
+  constructor(
+    private myRoute: Router,
+    private sessionService: SessionService,
+    private cookieService: CookieService
+  ) {}
+  public fingerPrint;
 
   sendToken(token) {
-    this.cookieService.set('JSESSIONID', token);
+    this.cookieService.set("JSESSIONID", token);
   }
 
   getToken() {
-    return this.cookieService.check('JSESSIONID');
+    return this.cookieService.check("JSESSIONID");
   }
 
   isLoggedIn() {
@@ -27,17 +29,23 @@ export class AuthService {
   }
 
   logout() {
-    this.sessionService.session()
-    .subscribe(res => {
-      this.sessionService.deleteSession(res.sessionId)
-      .subscribe(response => {
-        deleteFromStorage('user');
-        deleteFromStorage('provider');
-        deleteFromStorage('visitNoteProvider');
-        deleteFromStorage('session');
+    this.sessionService.session().subscribe((res) => {
+      this.sessionService.deleteSession(res.sessionId).subscribe((response) => {
+        deleteFromStorage("user");
+        deleteFromStorage("provider");
+        deleteFromStorage("visitNoteProvider");
+        deleteFromStorage("session");
         this.cookieService.deleteAll();
-        this.myRoute.navigate(['/login']);
+        this.myRoute.navigate(["/login"]);
+      });
     });
-    });
+  }
+
+  getFingerPrint() {
+    (async () => {
+      const fp = await FingerprintJS.load();
+      const result = await fp.get();
+      this.fingerPrint = result.visitorId;
+    })();
   }
 }
