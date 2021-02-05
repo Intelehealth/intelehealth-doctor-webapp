36341a641f28111f751463f2fe5fc3cf98fd0c66
shubham.jo <shubham@intelehealth.io>
2021-02-05 16:12:20 +0530

    Webapp  redirect  to profile page once Doctor is logged in first time

--- src/app/component/login-page/login-page.component.ts
+++ src/app/component/login-page/login-page.component.ts
@@ -4,8 +4,8 @@ import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { AuthService } from 'src/app/services/auth.service';
 import { SessionService } from 'src/app/services/session.service';
-declare var saveToStorage: any;
-
+// declare var saveToStorage: any;
+declare var getFromStorage: any, saveToStorage: any, deleteFromStorage: any;
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -43,12 +43,21 @@ export class LoginPageComponent implements OnInit {
     saveToStorage('session', base64);
     this.sessionService.loginSession(base64).subscribe(response => {
       if (response.authenticated === true) {
-        this.router.navigate(['/home']);
-        this.authService.sendToken(response.sessionId);
+        this.sessionService.provider(response.user.uuid).subscribe((provider) => {
+          if (provider.results[0].attributes.length === 0) {
+            this.router.navigate(['/myAccount']);
+          } else {
+            this.router.navigate(['/home']);
+          }
+
+        }, (error) => {
+          this.router.navigate(['home']);
+        })
+        this.authService.sendToken(response.user.sessionId);
         saveToStorage('user', response.user);
-        this.snackbar.open(`Welcome ${response.user.person.display}`, null, {duration: 4000});
+        this.snackbar.open(`Welcome ${response.user.person.display}`, null, { duration: 4000 });
       } else {
-        this.snackbar.open('Username & Password doesn\'t match', null, {duration: 4000});
+        this.snackbar.open('Username & Password doesn\'t match', null, { duration: 4000 });
       }
     });
   }
--- src/app/component/my-account/edit-details/edit-details.component.ts
+++ src/app/component/my-account/edit-details/edit-details.component.ts
@@ -50,9 +50,9 @@ export class EditDetailsComponent implements OnInit {
     private http: HttpClient,
     private dialog: MatDialog,
     private router: Router
-  ) {}
+  ) { }
 
-  ngOnInit() {      
+  ngOnInit() {
     this.userDetails = getFromStorage('user');
   }
 
@@ -62,16 +62,12 @@ export class EditDetailsComponent implements OnInit {
 
   editSignature() {
     var obj = {
-        name: this.data.textOfSign.value,
-        textOfSignuuid: this.data.textOfSign.uuid,
-        font: this.data.fontOfSign.value,
-        fontOfSignuuid:this.data.fontOfSign.uuid,
-        pid: this.data.uuid,
-        type: "edit"
+      pid: this.data.uuid,
+      type: "edit"
     }
-    this.dialog.open(SignatureComponent, { width: "500px", data: obj});
+    this.dialog.open(SignatureComponent, { width: "500px", data: obj });
   }
-  
+
   updateDetails() {
     const value = this.editForm.value;
     if (value.gender !== null && value.gender !== this.data.person.gender) {
@@ -79,7 +75,7 @@ export class EditDetailsComponent implements OnInit {
       const json = {
         gender: value.gender,
       };
-      this.http.post(URL, json).subscribe((response) => {});
+      this.http.post(URL, json).subscribe((response) => { });
     }
 
     if (value.emailId !== null) {
@@ -90,7 +86,7 @@ export class EditDetailsComponent implements OnInit {
         attributeType: "226c0494-d67e-47b4-b7ec-b368064844bd",
         value: value.emailId,
       };
-      this.http.post(URL, json).subscribe((response) => {});
+      this.http.post(URL, json).subscribe((response) => { });
     }
 
     if (value.phoneNphoneNphoneNumberumberphoneNumberumber !== null) {
@@ -101,7 +97,7 @@ export class EditDetailsComponent implements OnInit {
         attributeType: "e3a7e03a-5fd0-4e6c-b2e3-938adb3bbb37",
         value: value.phoneNumber.toString(),
       };
-      this.http.post(URL, json).subscribe((response) => {});
+      this.http.post(URL, json).subscribe((response) => { });
     }
 
     if (value.whatsapp !== null) {
@@ -112,18 +108,18 @@ export class EditDetailsComponent implements OnInit {
         attributeType: "fccc49f1-49ca-44bb-9e61-21c88ae6dd64",
         value: value.whatsapp.toString(),
       };
-      this.http.post(URL, json).subscribe((response) => {});
+      this.http.post(URL, json).subscribe((response) => { });
     }
 
     if (value.qualification !== null) {
       const URL = this.data.qualification
-      ? `${this.baseURLProvider}/${this.data.qualification.uuid}`
-      : this.baseURLProvider;
+        ? `${this.baseURLProvider}/${this.data.qualification.uuid}`
+        : this.baseURLProvider;
       const json = {
         attributeType: "4246639f-e9a8-48ea-b9ff-629a7c430543",
         value: value.qualification,
       };
-      this.http.post(URL, json).subscribe((response) => {});
+      this.http.post(URL, json).subscribe((response) => { });
     }
 
     if (value.registrationNumber !== null) {
@@ -133,8 +129,8 @@ export class EditDetailsComponent implements OnInit {
       const json = {
         attributeType: "992ccbdd-201a-44ef-8abb-c2eee079886d",
         value: value.registrationNumber,
-      }; 
-      this.http.post(URL, json).subscribe((response) => {});
+      };
+      this.http.post(URL, json).subscribe((response) => { });
     }
 
     if (value.signature !== null) {
@@ -156,10 +152,10 @@ export class EditDetailsComponent implements OnInit {
         attributeType: "ed1715f5-93e2-404e-b3c9-2a2d9600f062",
         value: value.specialization,
       };
-      this.http.post(URL, json).subscribe((response) => {});
+      this.http.post(URL, json).subscribe((response) => { });
     }
     this.onClose();
     setTimeout(() => window.location.reload(), 2000);
   }
-  
+
 }
