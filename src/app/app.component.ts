a3497b0275095df629c2271fa49175b07aa08b12
shubham.jo <shubham@intelehealth.io>
2021-03-09 19:17:14 +0530

    Updated PWA title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,4 @@
-import { SessionService } from "./services/session.service";
-import { VisitService } from "./services/visit.service";
 import { Component, OnInit } from "@angular/core";
-import { AuthService } from "./services/auth.service";
-import * as introJs from "intro.js/intro.js";
-import { Router } from "@angular/router";
-import { PushNotificationsService } from "./services/push-notification.service";
-import { SwPush, SwUpdate } from "@angular/service-worker";
-declare var getFromStorage: any, saveToStorage: any;
 
 @Component({
   selector: "app-root",
