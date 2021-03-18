7c933c63a5a4a73b02b7ba93ee74a99ee9e1bb8a
Zeeshan <zeeshan@intelehealth.org>
2021-03-18 13:21:27 +0530

    Hidding refer specialist on start visit

--- src/app/component/visit-summary/visit-summary.component.ts
+++ src/app/component/visit-summary/visit-summary.component.ts
@@ -1,17 +1,19 @@
-import { PushNotificationsService } from 'src/app/services/push-notification.service';
-import { VisitService } from '../../services/visit.service';
-import { Component, OnInit } from '@angular/core';
-import { EncounterService } from 'src/app/services/encounter.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { AuthService } from 'src/app/services/auth.service';
-import { DiagnosisService } from 'src/app/services/diagnosis.service';
-declare var getFromStorage: any, saveToStorage: any, getEncounterProviderUUID: any;
+import { PushNotificationsService } from "src/app/services/push-notification.service";
+import { VisitService } from "../../services/visit.service";
+import { Component, OnInit } from "@angular/core";
+import { EncounterService } from "src/app/services/encounter.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { AuthService } from "src/app/services/auth.service";
+import { DiagnosisService } from "src/app/services/diagnosis.service";
+declare var getFromStorage: any,
+  saveToStorage: any,
+  getEncounterProviderUUID: any;
 
 @Component({
-  selector: 'app-visit-summary',
-  templateUrl: './visit-summary.component.html',
-  styleUrls: ['./visit-summary.component.css']
+  selector: "app-visit-summary",
+  templateUrl: "./visit-summary.component.html",
+  styleUrls: ["./visit-summary.component.css"],
 })
 export class VisitSummaryComponent implements OnInit {
   show = false;
@@ -37,22 +39,23 @@ export class VisitSummaryComponent implements OnInit {
     "07a816ce-ffc0-49b9-ad92-a1bf9bf5e2ba",
     "e1761e85-9b50-48ae-8c4d-e6b7eeeba084",
     "3edb0e09-9135-481e-b8f0-07a26fa9a5ce",
-    "d63ae965-47fb-40e8-8f08-1f46a8a60b2b"
+    "d63ae965-47fb-40e8-8f08-1f46a8a60b2b",
   ];
 
-
-constructor(private service: EncounterService,
-            private visitService: VisitService,
-            private authService: AuthService,
-            private snackbar: MatSnackBar,
-            private route: ActivatedRoute,
-            private router: Router,
-            private diagnosisService: DiagnosisService,
-            private pushNotificationService: PushNotificationsService) {
-              this.router.routeReuseStrategy.shouldReuseRoute = function() {
-                return false;
-            };
-            }
+  constructor(
+    private service: EncounterService,
+    private visitService: VisitService,
+    private authService: AuthService,
+    private snackbar: MatSnackBar,
+    private route: ActivatedRoute,
+    private router: Router,
+    private diagnosisService: DiagnosisService,
+    private pushNotificationService: PushNotificationsService
+  ) {
+    this.router.routeReuseStrategy.shouldReuseRoute = function () {
+      return false;
+    };
+  }
 
   ngOnInit() {
     setTimeout(() => {
@@ -61,19 +64,17 @@ constructor(private service: EncounterService,
 
     this.visitUuid = this.route.snapshot.paramMap.get("visit_id");
     this.patientId = this.route.snapshot.params["patient_id"];
-    this.diagnosisService
-      .getObsAll(this.patientId)
-      .subscribe((response) => {
-      const ObsData = response.results.filter(a=>this.conceptIds.includes(a.concept.uuid))
-      console.log('ObsData: ', ObsData.length);
-      if(ObsData.length>0){
-        this.diagnosisService.isVisitSummaryChanged = true
+    this.diagnosisService.getObsAll(this.patientId).subscribe((response) => {
+      const ObsData = response.results.filter((a) =>
+        this.conceptIds.includes(a.concept.uuid)
+      );
+      console.log("ObsData: ", ObsData.length);
+      if (ObsData.length > 0) {
+        this.diagnosisService.isVisitSummaryChanged = true;
+      } else {
+        this.diagnosisService.isVisitSummaryChanged = false;
       }
-      else{
-        this.diagnosisService.isVisitSummaryChanged = false
-      }
-       
-     });
+    });
     const visitUuid = this.route.snapshot.paramMap.get("visit_id");
     this.visitService.fetchVisitDetails(visitUuid).subscribe((visitDetails) => {
       visitDetails.encounters.forEach((visit) => {
@@ -82,12 +83,13 @@ constructor(private service: EncounterService,
           this.visitNotePresent = true;
           this.show = true;
         }
-        if (visit.display.match('Visit Complete') !== null) {
+        if (visit.display.match("Visit Complete") !== null) {
           this.visitCompletePresent = true;
-          visit.encounterProviders[0].provider.attributes.forEach(element => {
-            if (element.attributeType.display === 'textOfSign') {
+          visit.encounterProviders[0].provider.attributes.forEach((element) => {
+            if (element.attributeType.display === "textOfSign") {
               this.text = element.value;
-            } if (element.attributeType.display === 'fontOfSign') {
+            }
+            if (element.attributeType.display === "fontOfSign") {
               this.font = element.value;
             }
           });
@@ -102,142 +104,176 @@ constructor(private service: EncounterService,
 
   onStartVisit() {
     const myDate = new Date(Date.now() - 30000);
-    const patientUuid = this.route.snapshot.paramMap.get('patient_id');
-    const visitUuid = this.route.snapshot.paramMap.get('visit_id');
+    const patientUuid = this.route.snapshot.paramMap.get("patient_id");
+    const visitUuid = this.route.snapshot.paramMap.get("visit_id");
     if (!this.visitNotePresent) {
-      const userDetails = getFromStorage('user');
-      const providerDetails = getFromStorage('provider');
+      const userDetails = getFromStorage("user");
+      const providerDetails = getFromStorage("provider");
       const attributes = providerDetails.attributes;
       if (userDetails && providerDetails) {
         const providerUuid = providerDetails.uuid;
         const json = {
           patient: patientUuid,
-          encounterType: 'd7151f82-c1f3-4152-a605-2f9ea7414a79',
-          encounterProviders: [{
-            provider: providerUuid,
-            encounterRole: '73bbb069-9781-4afc-a9d1-54b6b2270e03'
-          }],
+          encounterType: "d7151f82-c1f3-4152-a605-2f9ea7414a79",
+          encounterProviders: [
+            {
+              provider: providerUuid,
+              encounterRole: "73bbb069-9781-4afc-a9d1-54b6b2270e03",
+            },
+          ],
           visit: visitUuid,
-          encounterDatetime: myDate
+          encounterDatetime: myDate,
         };
-        this.service.postEncounter(json)
-        .subscribe(response => {
+        this.service.postEncounter(json).subscribe((response) => {
           if (response) {
-            this.visitService.fetchVisitDetails(visitUuid)
-            .subscribe(visitDetails => { saveToStorage('visitNoteProvider', visitDetails.encounters[0]); });
+            this.visitNotePresent = true;
+            this.visitService
+              .fetchVisitDetails(visitUuid)
+              .subscribe((visitDetails) => {
+                saveToStorage("visitNoteProvider", visitDetails.encounters[0]);
+              });
             this.show = true;
             this.snackbar.open(`Visit Note Created`, null, { duration: 4000 });
             attributes.forEach((element) => {
               if (
                 element.attributeType.uuid ===
-                "ed1715f5-93e2-404e-b3c9-2a2d9600f062" &&
+                  "ed1715f5-93e2-404e-b3c9-2a2d9600f062" &&
                 !element.voided
               ) {
                 const payload = {
                   speciality: element.value,
                   patient: {
                     name: response.patient.display,
-                    provider: response.encounterProviders[0].display
+                    provider: response.encounterProviders[0].display,
                   },
-                  skipFlag: true
+                  skipFlag: true,
                 };
-                if(!this.pushNotificationService.snoozeTimeout){
-                  this.pushNotificationService.postNotification(payload).subscribe();
+                if (!this.pushNotificationService.snoozeTimeout) {
+                  this.pushNotificationService
+                    .postNotification(payload)
+                    .subscribe();
                 }
               }
             });
           } else {
-            this.snackbar.open(`Visit Note Not Created`, null, {duration: 4000});
+            this.snackbar.open(`Visit Note Not Created`, null, {
+              duration: 4000,
+            });
           }
           this.diagnosisService.isVisitSummaryChanged = false;
         });
-      } else {this.authService.logout(); }
+      } else {
+        this.authService.logout();
+      }
     }
   }
 
   sign() {
     this.visitUuid = this.route.snapshot.paramMap.get("visit_id");
     this.patientId = this.route.snapshot.params["patient_id"];
-    this.diagnosisService
-      .getObsAll(this.patientId)
-      .subscribe((response) => {
-        if (response) {
-          this.signandsubmit();
-        }
-       
-      });
+    this.diagnosisService.getObsAll(this.patientId).subscribe((response) => {
+      console.log("response: ", response);
+      if (response) {
+        this.signandsubmit();
+      }
+    });
   }
 
   signandsubmit() {
     const myDate = new Date(Date.now() - 30000);
-    const patientUuid = this.route.snapshot.paramMap.get('patient_id');
-    const visitUuid = this.route.snapshot.paramMap.get('visit_id');
-    const userDetails = getFromStorage('user');
-    const providerDetails = getFromStorage('provider');
+    const patientUuid = this.route.snapshot.paramMap.get("patient_id");
+    const visitUuid = this.route.snapshot.paramMap.get("visit_id");
+    const userDetails = getFromStorage("user");
+    const providerDetails = getFromStorage("provider");
     if (userDetails && providerDetails) {
-        this.doctorDetails = providerDetails;
-        this.getDoctorValue();
-        const providerUuid = providerDetails.uuid;
-        if (providerUuid === getEncounterProviderUUID()) {
-        this.service.signRequest(providerUuid)
-        .subscribe(res => {
+      this.doctorDetails = providerDetails;
+      this.getDoctorValue();
+      const providerUuid = providerDetails.uuid;
+      if (providerUuid === getEncounterProviderUUID()) {
+        this.service.signRequest(providerUuid).subscribe((res) => {
           if (res.results.length) {
-            res.results.forEach(element => {
-              if (element.attributeType.display === 'textOfSign') {
+            res.results.forEach((element) => {
+              if (element.attributeType.display === "textOfSign") {
                 this.text = element.value;
-              } if (element.attributeType.display === 'fontOfSign') {
+              }
+              if (element.attributeType.display === "fontOfSign") {
                 this.font = element.value;
               }
             });
             const json = {
               patient: patientUuid,
-              encounterType: 'bd1fbfaa-f5fb-4ebd-b75c-564506fc309e',
-              encounterProviders: [{
-                provider: providerUuid,
-                encounterRole: '73bbb069-9781-4afc-a9d1-54b6b2270e03'
-                }],
+              encounterType: "bd1fbfaa-f5fb-4ebd-b75c-564506fc309e",
+              encounterProviders: [
+                {
+                  provider: providerUuid,
+                  encounterRole: "73bbb069-9781-4afc-a9d1-54b6b2270e03",
+                },
+              ],
               visit: visitUuid,
               encounterDatetime: myDate,
-              obs: [{
-                concept: '7a9cb7bc-9ab9-4ff0-ae82-7a1bd2cca93e',
-                value: JSON.stringify(this.doctorValue)
-              }],
+              obs: [
+                {
+                  concept: "7a9cb7bc-9ab9-4ff0-ae82-7a1bd2cca93e",
+                  value: JSON.stringify(this.doctorValue),
+                },
+              ],
             };
-            this.service.postEncounter(json)
-            .subscribe(post => { 
+            this.service.postEncounter(json).subscribe((post) => {
               this.visitCompletePresent = true;
-              this.router.navigateByUrl('/home');
-              this.snackbar.open('Visit Complete', null, {duration: 4000});
+              this.router.navigateByUrl("/home");
+              this.snackbar.open("Visit Complete", null, { duration: 4000 });
             });
           } else {
-            if (window.confirm('Your signature is not setup! If you click "Ok" you would be redirected. Cancel will load this website ')) {
-              this.router.navigateByUrl('/myAccount');
+            if (
+              window.confirm(
+                'Your signature is not setup! If you click "Ok" you would be redirected. Cancel will load this website '
+              )
+            ) {
+              this.router.navigateByUrl("/myAccount");
             }
           }
         });
-      } else {this.snackbar.open('Another doctor is viewing this case', null, {duration: 4000}); }
-    } else {this.authService.logout(); }
+      } else {
+        this.snackbar.open("Another doctor is viewing this case", null, {
+          duration: 4000,
+        });
+      }
+    } else {
+      this.authService.logout();
+    }
   }
 
   getDoctorValue = () => {
     const doctor = {};
-    doctor['name'] = this.doctorDetails.person.display;
+    doctor["name"] = this.doctorDetails.person.display;
     // tslint:disable-next-line: max-line-length
-    const doctorAttributes = ['phoneNumber', 'qualification', 'whatsapp', 'emailId', 'registrationNumber', 'specialization', 'address', 'fontOfSign', 'textOfSign'];
-    doctorAttributes.forEach(attr => {
-      const details = this.filterAttributes(this.doctorDetails.attributes, attr);
+    const doctorAttributes = [
+      "phoneNumber",
+      "qualification",
+      "whatsapp",
+      "emailId",
+      "registrationNumber",
+      "specialization",
+      "address",
+      "fontOfSign",
+      "textOfSign",
+    ];
+    doctorAttributes.forEach((attr) => {
+      const details = this.filterAttributes(
+        this.doctorDetails.attributes,
+        attr
+      );
       if (details.length) {
-        doctor[attr] = details[details.length - 1 ].value;
+        doctor[attr] = details[details.length - 1].value;
       }
     });
     this.doctorValue = doctor;
-  }
+  };
 
-
- filterAttributes = (data, text) => {
-    return data.filter(attr => attr.attributeType['display'].toLowerCase() === text.toLowerCase());
-  }
-
-
+  filterAttributes = (data, text) => {
+    return data.filter(
+      (attr) =>
+        attr.attributeType["display"].toLowerCase() === text.toLowerCase()
+    );
+  };
 }
-
