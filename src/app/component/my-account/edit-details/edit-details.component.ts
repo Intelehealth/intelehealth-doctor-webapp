e537725456c20612f60b9b4eae40005d487a7802
shubham.jo <shubham@intelehealth.io>
2021-04-23 12:02:36 +0530

    Added Speciality

--- src/app/component/my-account/edit-details/edit-details.component.ts
+++ src/app/component/my-account/edit-details/edit-details.component.ts
@@ -16,7 +16,11 @@ declare var getFromStorage: any;
 export class EditDetailsComponent implements OnInit {
   baseURL = environment.baseURL;
   baseURLProvider = `${this.baseURL}/provider/${this.data.uuid}/attribute`;
-  specializations = ["General Physician"];
+  specializations = [
+                      "General Physician",
+                      "Homeopathy",
+                      "Ayurveda"
+                    ];
   editForm = new FormGroup({
     gender: new FormControl(this.data.person ? this.data.person.gender : null),
     phoneNumber: new FormControl(
