d4b3d1f4c1552e84b21d1db762eee157b61fd7d5
dhanashri.p <dhanashri@intelehealth.org>
2021-04-30 12:15:10 +0530

    Date-range filter for only completed visits

--- src/app/component/homepage/tables/tables.component.ts
+++ src/app/component/homepage/tables/tables.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-tables',
@@ -14,19 +16,41 @@ export class TablesComponent implements OnInit {
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
   @Input() data;
-  constructor() { }
+  @Input() isCompletedVisit: boolean;
+  
+  // date-range filter form.
+  filterForm = new FormGroup({ fromDate: new FormControl(), toDate: new FormControl()});
+  get fromDate() { return this.filterForm.get('fromDate').value; }
+  get toDate() { return this.filterForm.get('toDate').value; }
+
+  constructor(private snackbar: MatSnackBar) {
+  }
 
   ngOnInit() {
     this.dataSource = new MatTableDataSource(this.data);
+    this.dataSource.filterPredicate = (data, filter) => {
+      if (typeof filter === 'number' && this.fromDate && this.toDate) {
+        return new Date(data.lastSeen) >= this.fromDate && new Date(data.lastSeen) <= this.toDate;
+      }
+      if( typeof filter === 'string') {
+        return data.name.toLowerCase().includes(filter) ||  data.id.toLowerCase().includes(filter) ||
+        data.provider.toLowerCase().includes(filter)  ;
+      }
+    }
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
 
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
+  //To apply date filter on data
+  applyDateFilter() {
+    if(this.fromDate && this.toDate) {
+      this.dataSource.filter = Math.random();
+    } else {
+      this.snackbar.open('Please select start and end date', null, { duration: 4000 });
     }
   }
 
