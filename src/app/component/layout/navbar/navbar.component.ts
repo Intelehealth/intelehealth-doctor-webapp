9241280ae598163c1711f70ddbbeeda6d6b24e39
shubham.jo <shubham@intelehealth.io>
2021-04-30 11:02:29 +0530

    Added Intelehealth logo and icons

--- src/app/component/layout/navbar/navbar.component.ts
+++ src/app/component/layout/navbar/navbar.component.ts
@@ -37,7 +37,8 @@ export class NavbarComponent implements OnInit {
   ];
   readonly VapidKEY =
     // "BAfolLQ7VpRSmWm6DskG-YyG3jjzq5z0rjKEl5HXLCw2W8CKS9cVmifnCAWnrlJMETgbgjuV1pWKLUf8zlbojH0"; // new
-    "BDGWYaKQhSDtC8VtcPekovFWM4M7mhs3NHe-X1HA7HH-t7nkiexSyYxUxQkwl2H44BiojKJjOdXi367XgxXxvpw" //myTeleDoc
+    // "BDGWYaKQhSDtC8VtcPekovFWM4M7mhs3NHe-X1HA7HH-t7nkiexSyYxUxQkwl2H44BiojKJjOdXi367XgxXxvpw" //myTeleDoc
+    "BN8YOCnlw8m1dZNYnDNvPdDX1NFpKCvRdSy_G9v2mU_2Oe3jurV5opRGVDTG7c7kDv-UGw1X64QQjOINpwxruHA" //cibid
     // "BFwuhYcJpWKFnTewNm9XtBTycAV_qvBqvIfbALC02CtOaMeXwrO6Zhm7MI_NIjDV9_TCbrr0FMmaDnZ7jllV6Xg"; //old
     // "BGg2p-PUsSzVF-_DgnNfTPTtnel4-oX7Z6lHT7BnDv88D-SffP_dj1XFVV_r0CsUKz59HmaJp8JadZuHNzzWyzs"; //testing
 
