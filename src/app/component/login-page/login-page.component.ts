eee3dfa0dfc8087e1b07c605d39228afc491cf7f
Zeeshan <zeeshan@intelehealth.org>
2021-06-15 10:32:19 +0530

    Added fallback to show welcome message

--- src/app/component/login-page/login-page.component.ts
+++ src/app/component/login-page/login-page.component.ts
@@ -1,20 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { AuthService } from 'src/app/services/auth.service';
-import { SessionService } from 'src/app/services/session.service';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { AuthService } from "src/app/services/auth.service";
+import { SessionService } from "src/app/services/session.service";
 declare var saveToStorage: any;
 
 @Component({
-  selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+  selector: "app-login-page",
+  templateUrl: "./login-page.component.html",
+  styleUrls: ["./login-page.component.css"],
 })
 export class LoginPageComponent implements OnInit {
   loginForm = new FormGroup({
-    username: new FormControl('', [Validators.required]),
-    password: new FormControl('', [Validators.required])
+    username: new FormControl("", [Validators.required]),
+    password: new FormControl("", [Validators.required]),
   });
 
   fieldTextType: boolean;
@@ -23,12 +23,12 @@ export class LoginPageComponent implements OnInit {
     private router: Router,
     private snackbar: MatSnackBar,
     private authService: AuthService
-  ) { }
+  ) {}
 
   ngOnInit() {
     const isLoggedIn: boolean = this.authService.isLoggedIn();
     if (isLoggedIn) {
-      this.router.navigateByUrl('/home');
+      this.router.navigateByUrl("/home");
     }
   }
 
@@ -40,14 +40,18 @@ export class LoginPageComponent implements OnInit {
     const value = this.loginForm.value;
     const string = `${value.username}:${value.password}`;
     const base64 = btoa(string);
-    this.sessionService.loginSession(base64).subscribe(response => {
+    this.sessionService.loginSession(base64).subscribe((response) => {
       if (response.authenticated === true) {
-        this.router.navigate(['/home']);
+        this.router.navigate(["/home"]);
         this.authService.sendToken(response.sessionId);
-        saveToStorage('user', response.user);
-        this.snackbar.open(`Welcome ${response.user.person.display}`, null, { duration: 4000 });
+        saveToStorage("user", response.user);
+        const display =
+          response?.user?.display || response?.user?.person?.display;
+        this.snackbar.open(`Welcome ${display}`, null, { duration: 4000 });
       } else {
-        this.snackbar.open('Username & Password doesn\'t match', null, { duration: 4000 });
+        this.snackbar.open("Username & Password doesn't match", null, {
+          duration: 4000,
+        });
       }
     });
   }
