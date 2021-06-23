6c1eb4f52c79cd67010af0cc5c34f8af2662cb58
dhanashri.p <dhanashri@intelehealth.org>
2021-06-23 14:12:29 +0530

    Fixes for EAH-38

--- src/app/component/visit-summary/patientinfo/patientinfo.component.ts
+++ src/app/component/visit-summary/patientinfo/patientinfo.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from "@angular/core";
-import { ImagesService } from "src/app/services/images.service";
 import { ActivatedRoute } from "@angular/router";
 import { VisitService } from "src/app/services/visit.service";
 import { environment } from "../../../../environments/environment";
-import * as moment from "moment";
 import { DatePipe } from "@angular/common";
 
 @Component({
@@ -14,31 +12,17 @@ import { DatePipe } from "@angular/common";
 })
 export class PatientinfoComponent implements OnInit {
   baseURL = environment.baseURL;
-  image: string;
   patientInfo = [];
   patientIdentifier: string;
   info = {};
   state: string;
-  profileImagePresent = false;
-  personAge: any;
-  yearAge: any;
-  age: any = {};
-  now: any;
-  a: any;
 
   constructor(
     private route: ActivatedRoute,
-    private visitService: VisitService,
-    private service: ImagesService,
-    private datePipe: DatePipe
-  ) {}
+    private visitService: VisitService) {}
 
   ngOnInit() {
     const uuid = this.route.snapshot.paramMap.get("patient_id");
-    this.service.fetchProfileImage(uuid).subscribe((response) => {
-      this.profileImagePresent = true;
-      this.image = `${this.baseURL}/personimage/${uuid}`;
-    });
     this.visitService.patientInfo(uuid).subscribe((info) => {
       this.info = info.person;
       this.state = info.person.preferredAddress.stateProvince
@@ -50,28 +34,5 @@ export class PatientinfoComponent implements OnInit {
       });
       this.patientInfo.push(this.info);
     });
-  }
-
-  getAge(dateString) {
-    //------sol 1 ---------------
-    var mydate = dateString.replace(
-      /^(\d{2})\/(\d{2})\/(\d{4})$/,
-      "$3, $2, $1"
-    );
-
-    this.now = new Date();
-    var todayDate = this.datePipe.transform(this.now, "yyyy, MM, dd");
-
-    var a = moment(todayDate);
-    var b = moment(mydate);
-    var diffDuration = moment.duration(a.diff(b));
-    var ageString =
-      diffDuration.years() +
-      " years - " +
-      diffDuration.months() +
-      " months - " +
-      diffDuration.days() +
-      " days";
-    return ageString;
   }
 }
