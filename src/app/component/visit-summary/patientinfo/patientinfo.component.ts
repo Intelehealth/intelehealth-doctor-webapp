fc1e19fa09e719685d321a1dc8375ed269209353
shubham.jo <shubham@intelehealth.org>
2021-06-29 18:24:07 +0530

    Added Patient documents in patient info

--- src/app/component/visit-summary/patientinfo/patientinfo.component.ts
+++ src/app/component/visit-summary/patientinfo/patientinfo.component.ts
@@ -4,6 +4,7 @@ import { VisitService } from 'src/app/services/visit.service';
 import { environment } from '../../../../environments/environment';
 import * as moment from "moment";
 import { DatePipe } from "@angular/common";
+import { DiagnosisService } from 'src/app/services/diagnosis.service';
 
 @Component({
   selector: 'app-patientinfo',
@@ -21,14 +22,32 @@ export class PatientinfoComponent implements OnInit {
   age: any = {};
   now: any;
   a: any;
+  images: any = [];
+  patientDocumentPresent = false;
+  conceptAdditionlDocument = '07a816ce-ffc0-49b9-ad92-a1bf9bf5e2ba';
 
   constructor(private route: ActivatedRoute,
     private visitService: VisitService,
-    private datePipe: DatePipe
-    ) { }
+    private datePipe: DatePipe,
+    private diagnosisService: DiagnosisService
+  ) { }
 
   ngOnInit() {
     const uuid = this.route.snapshot.paramMap.get('patient_id');
+    this.diagnosisService.getObs(uuid, this.conceptAdditionlDocument)
+      .subscribe(response => {
+        response.results.forEach(obs => {
+          // if (obs.encounter !== null && obs.encounter.visit.uuid === visitUuid) {
+          this.patientDocumentPresent = true;
+          const data = {
+            image: `${this.baseURL}/obs/${obs.uuid}/value`
+          };
+          this.images.push(data);
+          // }
+        });
+      });
+
+
     this.visitService.patientInfo(uuid)
       .subscribe(info => {
         this.info = info.person;
