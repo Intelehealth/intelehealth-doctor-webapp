9e05723a9258973c525540a53ff211fda1c96c50
jmshuff <37374473+jmshuff@users.noreply.github.com>
2021-09-14 11:29:25 -0400

    combine past medical history and family history

--- src/app/component/visit-summary/past-medical-history/past-medical-history.component.ts
+++ src/app/component/visit-summary/past-medical-history/past-medical-history.component.ts
@@ -11,6 +11,10 @@ export class PastMedicalHistoryComponent implements OnInit {
 pastMedical: any = [];
 pastMedicalHistoryPresent = false;
 conceptPastMedical = '62bff84b-795a-45ad-aae1-80e7f5163a82';
+  
+familyHistory: any = [];
+familyHistoryPresent = false;
+conceptFamilyHistory = 'd63ae965-47fb-40e8-8f08-1f46a8a60b2b';
 
   constructor(private diagnosisService: DiagnosisService,
               private route: ActivatedRoute) { }
@@ -19,6 +23,18 @@ conceptPastMedical = '62bff84b-795a-45ad-aae1-80e7f5163a82';
     const patientUuid = this.route.snapshot.paramMap.get('patient_id');
     const visitUuid = this.route.snapshot.paramMap.get('visit_id');
     this.diagnosisService.getObs(patientUuid, this.conceptPastMedical)
+    .subscribe(response => {
+      response.results.forEach(obs => {
+        if (obs.encounter.visit.uuid === visitUuid) {
+          this.pastMedical.push(obs);
+        }
+      });
+      if (this.pastMedical !== undefined) {
+        this.pastMedicalHistoryPresent = true;
+      }
+    });
+    
+    this.diagnosisService.getObs(patientUuid, this.conceptFamilyHistory)
     .subscribe(response => {
       response.results.forEach(obs => {
         if (obs.encounter.visit.uuid === visitUuid) {
