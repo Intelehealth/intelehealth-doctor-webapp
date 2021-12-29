cdfd6687e8049011cc9a8087e09803f4a7241e11
dhanashri.p <dhanashri@intelehealth.org>
2021-12-29 18:45:45 +0530

    Scheduling related changes

--- src/app/component/appointment/appointment.component.ts
+++ src/app/component/appointment/appointment.component.ts
@@ -7,6 +7,7 @@ import {
 import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
 import { MatSnackBar } from "@angular/material/snack-bar";
 import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { TranslateService } from "@ngx-translate/core";
 import { CalendarEvent, CalendarView } from "angular-calendar";
 import * as moment from "moment";
 import { AppointmentService } from "src/app/services/appointment.service";
@@ -41,19 +42,18 @@ export class AppointmentComponent implements OnInit {
   events: CalendarEvent[];
   activeDayIsOpen: boolean = false;
   selectedLang: string = 'en';
-  days = [];
+  selectedDays = [];
   scheduleForm = new FormGroup({
-    selectedDay: new FormControl('',[Validators.required]),
-    startTime: new FormControl('', [Validators.required]),
-    endTime: new FormControl('', [Validators.required])
+    startTime: new FormControl('9:00 AM', [Validators.required]),
+    endTime: new FormControl('6:00 PM', [Validators.required])
   });
-  userSchedule: any = null;
+  userSchedule: any = Object;
   type: string = 'month';
   types: any = [
     { day: "All Days", startTime: null, endTime: null, checked: false },
     { day: "Week Day", startTime: null, endTime: null, checked: false }
   ];
- weekDays: any = [
+  weekDays: any = [
     { day: "Monday", startTime: null, endTime: null, checked: false },
     { day: "Tuesday", startTime: null, endTime: null, checked: false },
     { day: "Wednesday", startTime: null, endTime: null, checked: false },
@@ -63,8 +63,8 @@ export class AppointmentComponent implements OnInit {
   weekends: any = [
     { day: "Saturday", startTime: null, endTime: null, checked: false },
     { day: "Sunday", startTime: null, endTime: null, checked: false }
-  ]; 
-  slotHours = [];
+  ];
+  slotHours = []; errorMsg: string = null;
   modalData: {
     date: Date;
     events: CalendarEvent[];
@@ -75,7 +75,8 @@ export class AppointmentComponent implements OnInit {
     private snackbar: MatSnackBar,
     private translationService: TranslationService,
     private modal: NgbModal,
-  ) {}
+    private translateService: TranslateService,
+  ) { }
 
   ngOnInit(): void {
     this.selectedLang = localStorage.getItem('selectedLanguage');
@@ -86,13 +87,18 @@ export class AppointmentComponent implements OnInit {
   getSchedule() {
     this.appointmentService.getUserAppoitment(this.userId).subscribe({
       next: (res: any) => {
-        this.userSchedule = res.data.slotSchedule;
-        this.initializeEvents(res.data.slotSchedule)
+        if (res && res.data) {
+          this.userSchedule = res.data;
+        } else {
+          this.userSchedule.slotSchedule = [];
+        }
+        this.initializeEvents(this.userSchedule.slotSchedule)
       },
     });
   }
 
-  private initializeEvents(slot) {
+  private initializeEvents(slots) {
+    let slot = slots.reverse().filter((set => f => !set.has(new Date(f.date).getTime()) && set.add(new Date(f.date).getTime()))(new Set));
     let array: CalendarEvent[] = [];
     for (let i = 0; i < slot.length; i++) {
       let event1 = {
@@ -101,7 +107,7 @@ export class AppointmentComponent implements OnInit {
         start: new Date(slot[i].date),
         startTime: slot[i].startTime,
         endTime: slot[i].endTime,
-        day:slot[i].day
+        day: slot[i].day
       };
       event1["isTimeOver"] = event1.start < new Date();
       array.push(event1);
@@ -167,29 +173,24 @@ export class AppointmentComponent implements OnInit {
   }
 
   addSchedule() {
+    this.clear();
+    this.selectedDays = [];
     this.modal.open(this.modalContent);
   }
 
   set(type) {
-    console.log(type)
+    this.errorMsg = null;
     if (type.day === "All Days") {
       this.type = 'month';
       this.weekDays.forEach(element => type.checked === true ? (element.checked = true) : (element.checked = false));
+      this.weekends.forEach(element => type.checked === true ? (element.checked = true) : (element.checked = false));
     } else if (type.day === "Week Day") {
       this.type = 'week';
-      let array = [];
-      for (let element of this.weekDays) {
-        if (type.checked === true && ['Week Day', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'].includes(element.day)) {
-          element.checked = true;
-        } else {
-          element.checked = false;
-        }
-        array.push(element);
-      };
-      this.weekDays = array;
+      this.weekDays.forEach(element => type.checked === true ? (element.checked = true) : (element.checked = false));
     } else {
       this.type = 'week';
     }
+    this.selectedDays = this.weekDays.filter((day) => day.checked).concat(this.weekends.filter((day) => day.checked))
   }
 
   getSlotSchedule(selectedDays) {
@@ -201,56 +202,71 @@ export class AppointmentComponent implements OnInit {
       while (month === day.month()) {
         day.add(7, 'd');
         schedules.push({
-          day:d.day,
-          endTime:this.scheduleForm.value.endTime,
-          startTime:this.scheduleForm.value.startTime,
+          day: d.day,
+          endTime: this.scheduleForm.value.endTime,
+          startTime: this.scheduleForm.value.startTime,
           date: day.toString()
-        })
+        });
       }
     }
-    return schedules;
+    let existing = this.userSchedule.slotSchedule;
+    let current = existing.concat(schedules)
+    return current;
   }
 
   submit() {
     if (this.validateTimeSlots(this.scheduleForm.value)) {
-      const selectedDays = this.weekDays.filter((day) => day.checked);
-      const speciality = this.getSpeciality();
-      let body = this.getJson(speciality, selectedDays)
-      this.appointmentService
-        .updateOrCreateAppointment(body).subscribe({
-          next: (res: any) => {
-            if (res.status) {
-              this.initializeEvents(body.slotSchedule)
-              this.translationService.getTranslation(res.message);
-            }
-          },
-        });
-      console.log(selectedDays, body)
+      if (this.selectedDays.length > 0) {
+        const speciality = this.getSpeciality();
+        let body = this.getJson(speciality)
+        this.saveSchedule(body);
+        this.modal.dismissAll();
+        this.errorMsg = null;
+      } else {
+        this.error('Select Day Message');
+      }
     } else {
-      this.translationService.getTranslation('Check Time Message');
+      this.error('Check Time Message');
     }
-    this.scheduleForm.reset();
   }
 
-  private getJson(speciality: any, selectedDays: any) {
+  private saveSchedule(body: { speciality: any; type: string; userUuid: any; slotDays: any; slotSchedule: any[]; drName: any; }) {
+    console.log("body", body)
+    this.appointmentService
+      .updateOrCreateAppointment(body).subscribe({
+        next: (res: any) => {
+          if (res.status) {
+            this.userSchedule = body;
+            this.initializeEvents(body.slotSchedule);
+            this.translationService.getTranslation(res.message);
+          }
+        },
+      });
+  }
+
+  private getJson(speciality: any) {
     return {
       speciality,
       type: this.type,
       userUuid: this.userId,
-      slotDays: selectedDays.map((d) => d.day).join("||"),
-      slotSchedule: this.getSlotSchedule(selectedDays),
+      slotDays: this.selectedDays.map((d) => d.day).join("||"),
+      slotSchedule: this.getSlotSchedule(this.selectedDays),
       drName: this.drName,
     };
   }
 
   clear() {
-    let array = [];
     this.weekDays.forEach(element => {
       element.checked = false;
-      array.push(element);
     });
-    this.weekDays = array;
-    console.log(array)
+    this.weekends.forEach(element => {
+      element.checked = false;
+    });
+    this.types.forEach(element => {
+      element.checked = false;
+    });
+    this.errorMsg = null;
+    this.selectedDays = [];
   }
 
   closeOpenMonthViewDay() {
@@ -259,17 +275,38 @@ export class AppointmentComponent implements OnInit {
 
   dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
     if (events.length !== 0) {
-    this.modalData = { date, events };
-    this.modal.open(this.schedule);
+      this.modalData = { date, events };
+      this.modal.open(this.schedule);
     }
   }
 
-  cancel(day) {
-    console.log(day,this.userSchedule)
+  update(selectedDay, operation) {
+    let array = this.userSchedule.slotSchedule;
+    array.forEach((element, index) => {
+      if (operation === 'cancel' && new Date(element.date).getTime() === new Date(selectedDay.start).getTime()) {
+        array.splice(index, 1);
+      }
+      if (operation === 'update' && new Date(element.date).getTime() === new Date(selectedDay.start).getTime()) {
+        element.startTime = selectedDay.startTime;
+        element.endTime = selectedDay.endTime;
+      }
+    });
+    this.userSchedule.slotSchedule = array;
+    let body = {
+      speciality: this.userSchedule.speciality,
+      type: this.userSchedule.type,
+      userUuid: this.userSchedule.userUuid,
+      slotDays: this.userSchedule.slotDays,
+      slotSchedule: this.userSchedule.slotSchedule,
+      drName: this.userSchedule.drName
+    };
+    this.saveSchedule(body);
   }
 
-  update(day) {
-    console.log(day)
+  private error(msg) {
+    this.translateService.get(`messages.${msg}`).subscribe((res: string) => {
+      this.errorMsg = res;
+    });
   }
 
 }
