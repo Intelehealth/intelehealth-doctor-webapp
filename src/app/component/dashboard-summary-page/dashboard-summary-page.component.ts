59bc62bada3785b38c73f33569988428ad611a89
Zeeshan <zeeshan@intelehealth.org>
2022-11-02 18:43:52 +0530

    -Added all the table using common components, added collapse button to collapse table

--- src/app/component/dashboard-summary-page/dashboard-summary-page.component.ts
+++ src/app/component/dashboard-summary-page/dashboard-summary-page.component.ts
@@ -8,8 +8,11 @@ import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 })
 export class DashboardSummaryPageComponent implements OnInit {
   appointmentTable: any = {
+    id: "appointmentTable",
     label: "Appointments",
     lableIconPath: "assets/svgs/video-frame.svg",
+    collapse: "#collapseAppointment",
+    toggle: "collapse",
     dataCount: 0,
     headers: [
       {
@@ -77,6 +80,194 @@ export class DashboardSummaryPageComponent implements OnInit {
     ],
   };
 
+  priorityVisits: any = {
+    id: "priorityTable",
+    label: "Priority Visits",
+    lableIconPath: "assets/svgs/red-profile.svg",
+    dataCount: 8,
+    headers: [
+      {
+        name: "Patient",
+        type: "stringwithimage",
+        key: "patientName",
+        imageKey: "profile",
+      },
+      { name: "Age", type: "string", key: "age" },
+      { name: "Location", type: "string", key: "location" },
+      { name: "Cheif complaint", type: "string", key: "complaint" },
+      {
+        name: "Visit Created",
+        type: "pill",
+        headerClass: "text-center",
+        imageKey: "summaryListIcon",
+
+        buttons: [
+          {
+            label: "1 hr ago",
+            btnClass: "summay-btn pill-btn",
+          },
+        ],
+      },
+    ],
+    data: [
+      {
+        patientName: "Muskan Kala",
+        age: "25y",
+        location: "TM clinic 1",
+        complaint: "Fever",
+        summaryListIcon: "assets/svgs/summary-list.svg",
+        isActive: true,
+      },
+      {
+        patientName: "Muskan Kala",
+        age: "25y",
+        location: "TM clinic 1",
+        complaint: "Fever",
+        summaryListIcon: "assets/svgs/summary-list.svg",
+      },
+      {
+        patientName: "Muskan Kala",
+        age: "25y",
+        location: "TM clinic 1",
+        complaint: "Headache",
+        summaryListIcon: "assets/svgs/summary-list.svg",
+      },
+      {
+        patientName: "Muskan Kala",
+        age: "25y",
+        location: "TM clinic 1",
+        complaint: "Fever",
+        summaryListIcon: "assets/svgs/summary-list.svg",
+      },
+    ],
+  };
+
+  awaitingVisits: any = {
+    id: "awaitingTable",
+    label: "Awaiting Visits",
+    lableIconPath: "assets/svgs/green-profile.svg",
+    dataCount: 12,
+    headers: [
+      {
+        name: "Patient",
+        type: "stringwithimage",
+        key: "patientName",
+        imageKey: "profile",
+      },
+      { name: "Age", type: "string", key: "age" },
+      { name: "Location", type: "string", key: "location" },
+      { name: "Cheif complaint", type: "string", key: "complaint" },
+      {
+        name: "Visit Created",
+        type: "pill",
+        headerClass: "text-center",
+        imageKey: "summaryListIcon",
+
+        buttons: [
+          {
+            label: "16 hr ago",
+
+            btnClass: "summay-btn pill-btn",
+          },
+        ],
+      },
+    ],
+    data: [
+      {
+        patientName: "Muskan Kala",
+        age: "25y",
+        location: "TM clinic 1",
+        complaint: "Fever",
+        summaryListIcon: "assets/svgs/summary-list.svg",
+        isActive: true,
+      },
+      {
+        patientName: "Muskan Kala",
+        age: "25y",
+        location: "TM clinic 1",
+        complaint: "Fever",
+        summaryListIcon: "assets/svgs/summary-list.svg",
+      },
+      {
+        patientName: "Muskan Kala",
+        age: "25y",
+        location: "TM clinic 1",
+        complaint: "Fever & Headache",
+        summaryListIcon: "assets/svgs/summary-list.svg",
+      },
+      {
+        patientName: "Muskan Kala",
+        age: "25y",
+        location: "TM clinic 1",
+        complaint: "Fever",
+        summaryListIcon: "assets/svgs/summary-list.svg",
+      },
+    ],
+  };
+
+  inProgressVisits: any = {
+    id: "inProgressTable",
+    label: "In-progress visits",
+    lableIconPath: "assets/svgs/pen-board.svg",
+    dataCount: 15,
+    headers: [
+      {
+        name: "Patient",
+        type: "stringwithimage",
+        key: "patientName",
+        imageKey: "profile",
+      },
+      { name: "Age", type: "string", key: "age" },
+      { name: "Location", type: "string", key: "location" },
+      { name: "Cheif complaint", type: "string", key: "complaint" },
+      {
+        name: "Prescription started",
+        type: "pill",
+        headerClass: "text-center",
+        imageKey: "summaryListIcon",
+
+        buttons: [
+          {
+            label: "16 hr ago",
+
+            btnClass: "summay-btn pill-btn",
+          },
+        ],
+      },
+    ],
+    data: [
+      {
+        patientName: "Muskan Kala",
+        age: "25y",
+        location: "TM clinic 1",
+        complaint: "Fever",
+        summaryListIcon: "assets/svgs/summary-list.svg",
+        isActive: true,
+      },
+      {
+        patientName: "Muskan Kala",
+        age: "27y",
+        location: "TM clinic 1",
+        complaint: "Fever",
+        summaryListIcon: "assets/svgs/summary-list.svg",
+      },
+      {
+        patientName: "Muskan Kala",
+        age: "25y",
+        location: "TM clinic 1",
+        complaint: "Fever & Headache",
+        summaryListIcon: "assets/svgs/summary-list.svg",
+      },
+      {
+        patientName: "Muskan Kala",
+        age: "29y",
+        location: "TM clinic 1",
+        complaint: "Fever & Cough",
+        summaryListIcon: "assets/svgs/summary-list.svg",
+      },
+    ],
+  };
+
   constructor() {}
 
   ngOnInit(): void {}
@@ -87,5 +278,15 @@ export class DashboardSummaryPageComponent implements OnInit {
 
   onCancelClick(item) {
     console.log("onCancelClick: ", item);
+  }
+
+  onVisitCreatedClick(item) {
+    console.log("onVisitCreatedClick", item);
+  }
+  onAwaitingVisitClick(item) {
+    console.log("onAwaitingVisitClick", item);
+  }
+  onProgressVisitClick(item) {
+    console.log("oninProgressVisitClick", item);
   }
 }
