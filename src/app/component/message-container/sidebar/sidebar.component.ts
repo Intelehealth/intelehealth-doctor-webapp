d44bde2688050fdd8cfa64840b94a2263f9ca2b0
VishalMajumdar09 <vishal@intelehealth.org>
2022-11-07 19:44:57 +0530

    IDA4-99 Added sidebar and chat area

---
+++ src/app/component/message-container/sidebar/sidebar.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { conversations } from './nav-data';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss'],
+})
+export class SidebarComponent implements OnInit {
+  @Output() conversationClicked: EventEmitter<any> = new EventEmitter();
+  searchText: string;
+  conversations = conversations;
+
+  get filteredConversations() {
+    return this.conversations.filter((conversation) => {
+      return (
+        conversation.name
+          .toLowerCase()
+          .includes(this.searchText.toLowerCase()) ||
+        conversation.latestMessage
+          .toLowerCase()
+          .includes(this.searchText.toLowerCase())
+      );
+    });
+  }
+  constructor() {}
+
+  ngOnInit(): void {}
+}
