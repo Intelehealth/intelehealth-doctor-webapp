a07afc400c9b650f13ff67d97d4cacb86e72ebd1
VishalMajumdar09 <vishal@intelehealth.org>
2022-11-15 19:01:58 +0530

    IDA4-98 added the call-state component

---
+++ src/app/component/call-state/call-state.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-call-state',
+  templateUrl: './call-state.component.html',
+  styleUrls: ['./call-state.component.scss']
+})
+export class CallStateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
