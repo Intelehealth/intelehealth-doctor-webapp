16dda3d5e97a96d0ad4e814e511cb5e0646e0692
Vishal Majumdar <vishal@intelehealth.org>
2022-12-12 21:40:50 +0530

    IDA4-300 completed the implementation

--- src/app/component/profile-container/personal-information/personal-information.component.ts
+++ src/app/component/profile-container/personal-information/personal-information.component.ts
@@ -1,20 +1,45 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit, AfterViewInit, } from '@angular/core';
 import SignaturePad from 'signature_pad';
+import { CountryData } from "../../country-data/country-data";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-personal-information',
   templateUrl: './personal-information.component.html',
   styleUrls: ['./personal-information.component.scss']
 })
-export class PersonalInformationComponent{
+export class PersonalInformationComponent implements OnInit, AfterViewInit{
   @ViewChild('canvas') canvasEl: ElementRef;
   signatureImg: string;
   signaturePad: SignaturePad;
   moveTo: boolean = false;
-  
-  constructor() { }
+  selectedLanguage: any;
+  languageList = [{ name: "English" }, { name: "Hindi" }];
+
+  countryCode: any = this.country.country_Codes.map((code: any) => {
+    code.img = `assets/flags/1x1/${code.country_code.toLowerCase()}.svg`;
+    return code;
+  });
+  default: string = "+91";
+
+  personalInfoForm = new FormGroup({
+    phoneNumber: new FormControl("", [Validators.required]),
+    email: new FormControl("", [Validators.required]),
+    selectedCode: new FormControl("", [Validators.required]),
+  });
+
+  constructor(
+    private country: CountryData,
+    private router: Router
+  ) {
+    this.personalInfoForm.controls["selectedCode"].setValue(this.default, {
+      onlySelf: true,
+    });
+   }
 
   ngOnInit(): void {
+    this.selectedLanguage = this.languageList[0];
   }
 
   ngAfterViewInit() {
