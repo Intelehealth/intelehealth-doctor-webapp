1acf5fb7935e50cb0b67d0bfc1abc7a4b562cce4
Vishal Majumdar <vishal@intelehealth.org>
2022-12-19 11:29:50 +0530

    Added language selector

---
+++ src/app/component/set-up-profile/select-language/select-language.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-select-language',
+  templateUrl: './select-language.component.html',
+  styleUrls: ['./select-language.component.scss']
+})
+export class SelectLanguageComponent implements OnInit {
+  active;
+  constructor(@Inject(MAT_DIALOG_DATA) public data,
+  private dialogRef: MatDialogRef<SelectLanguageComponent>) { }
+
+  ngOnInit(): void {
+  }
+
+  close(): void {
+    this.dialogRef.close();
+  }
+
+  activeSelection(): void {
+    this.active = "active";
+  }
+
+}
