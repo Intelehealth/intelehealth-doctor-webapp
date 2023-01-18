68cc9a81669c0c5e6080aa2244bfe3ea3c9638ae
Zeeshan <zeeshan@intelehealth.org>
2023-01-18 18:22:57 +0530

    webrtc call initiate changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: true,
-  base:`${window.location.protocol}//${window.location.host}`,
+  base: `${window.location.protocol}//${window.location.host}`,
   baseURL: `${window.location.protocol}//${window.location.host}/openmrs/ws/rest/v1`,
   baseURLCoreApp: `${window.location.protocol}//${window.location.host}/openmrs/coreapps/diagnoses`,
   baseURLLegacy: `${window.location.protocol}//${window.location.host}/openmrs`,
   mindmapURL: `${window.location.protocol}//${window.location.hostname}:3004/api`,
   notificationURL: `${window.location.protocol}//${window.location.hostname}:3004/notification`,
   socketURL: `${window.location.protocol}//${window.location.hostname}:3004`,
+  captchaSiteKey: "6LdUIXgjAAAAAJyQHOTzABeaNV0_LhKHtWULv63t",
   firebase: {
     /* apiKey: "AIzaSyC5cRqdDtLWwJpz7WY1Ekpx7rbawbG1CA8",
     authDomain: "intelehealth-3-0.firebaseapp.com",
