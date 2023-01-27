beaf79aba25078673c935297f209a479b3d2a209
Vishal Majumdar <vishal@intelehealth.org>
2023-01-27 18:11:19 +0530

    NAS-155 Added view photos functionality

--- src/app/component/visit-summary/additional-documents/additional-documents.component.ts
+++ src/app/component/visit-summary/additional-documents/additional-documents.component.ts
@@ -2,18 +2,42 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DiagnosisService } from 'src/app/services/diagnosis.service';
 import { environment } from '../../../../environments/environment';
+import { animate, style, transition, trigger, AnimationEvent } from '@angular/animations';
 
 
 @Component({
   selector: 'app-additional-documents',
   templateUrl: './additional-documents.component.html',
-  styleUrls: ['./additional-documents.component.css']
+  styleUrls: ['./additional-documents.component.scss'],
+  animations: [
+    trigger('animation', [
+      transition('void => visible', [
+        style({transform: 'scale(0.5)'}),
+        animate('150ms', style({transform: 'scale(1)'}))
+      ]),
+      transition('visible => void', [
+        style({transform: 'scale(1)'}),
+        animate('150ms', style({transform: 'scale(0.5)'}))
+      ]),
+    ]),
+    trigger('animation2', [
+      transition(':leave', [
+        style({opacity: 1}),
+        animate('50ms', style({opacity: 0.8}))
+      ])
+    ])
+  ]
 })
 export class AdditionalDocumentsComponent implements OnInit {
-baseURL = environment.baseURL;
-images: any = [];
-additionalDocumentPresent = false;
-conceptAdditionlDocument = '07a816ce-ffc0-49b9-ad92-a1bf9bf5e2ba';
+  baseURL = environment.baseURL;
+  images: any = [];
+  previewImage = false;
+  showMask = false;
+  currentLightboxImage: any = this.images[0];
+  currentIndex = 0;
+  totalImageCount = 0;
+  additionalDocumentPresent = false;
+  conceptAdditionlDocument = '07a816ce-ffc0-49b9-ad92-a1bf9bf5e2ba';
 
   constructor(private diagnosisService: DiagnosisService,
     private route: ActivatedRoute) { }
@@ -30,9 +54,43 @@ conceptAdditionlDocument = '07a816ce-ffc0-49b9-ad92-a1bf9bf5e2ba';
         image: `${this.baseURL}/obs/${obs.uuid}/value`
         };
         this.images.push(data);
+        this.totalImageCount = this.images.length;
       }
       });
     });
   }
+
+  onPreviewImage(index: number): void {
+    this.showMask = true;
+    this.previewImage = true;
+    this.currentIndex = index;
+    this.currentLightboxImage = this.images[index];
   }
+
+  onAnimationEnd(event: AnimationEvent) {
+    if(event.toState === 'void') {
+      this.showMask = false;
+    }
+  }
+
+  onClosePreview() {
+    this.previewImage = false;
+  }
+
+  next(): void {
+    this.currentIndex = this.currentIndex + 1;
+    if(this.currentIndex > this.images.length - 1) {
+      this.currentIndex = 0;
+    }
+    this.currentLightboxImage = this.images[this.currentIndex];
+  }
+
+  prev(): void {
+    this.currentIndex = this.currentIndex - 1;
+    if(this.currentIndex < 0) {
+      this.currentIndex = this.images.length - 1;
+    }
+    this.currentLightboxImage = this.images[this.currentIndex];
+  }
+}
 
--- src/app/component/visit-summary/physical-examination/physical-examination.component.ts
+++ src/app/component/visit-summary/physical-examination/physical-examination.component.ts
@@ -2,16 +2,41 @@ import { DiagnosisService } from 'src/app/services/diagnosis.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { environment } from '../../../../environments/environment';
+import { animate, style, transition, trigger, AnimationEvent } from '@angular/animations';
 
 
 @Component({
   selector: 'app-physical-examination',
   templateUrl: './physical-examination.component.html',
-  styleUrls: ['./physical-examination.component.css']
+  styleUrls: ['./physical-examination.component.scss'],
+  animations: [
+    trigger('animation', [
+      transition('void => visible', [
+        style({transform: 'scale(0.5)'}),
+        animate('150ms', style({transform: 'scale(1)'}))
+      ]),
+      transition('visible => void', [
+        style({transform: 'scale(1)'}),
+        animate('150ms', style({transform: 'scale(0.5)'}))
+      ]),
+    ]),
+    trigger('animation2', [
+      transition(':leave', [
+        style({opacity: 1}),
+        animate('50ms', style({opacity: 0.8}))
+      ])
+    ])
+  ]
 })
 export class PhysicalExaminationComponent implements OnInit {
   baseURL = environment.baseURL;
   images: any = [];
+  previewImage = false;
+  showMask = false;
+  currentLightboxImage: any = this.images[0];
+  currentIndex = 0;
+  totalImageCount = 0;
+
   physicalExamPresent = false;
   conceptPhysicalExamination = '200b7a45-77bc-4986-b879-cc727f5f7d5b';
 
@@ -30,8 +55,44 @@ export class PhysicalExaminationComponent implements OnInit {
             image: `${this.baseURL}/obs/${obs.uuid}/value`
             };
           this.images.push(data);
+          this.totalImageCount = this.images.length;
+          console.log(this.images,this.totalImageCount);
+          
         }
       });
     });
   }
+
+  onPreviewImage(index: number): void {
+    this.showMask = true;
+    this.previewImage = true;
+    this.currentIndex = index;
+    this.currentLightboxImage = this.images[index];
   }
+
+  onAnimationEnd(event: AnimationEvent) {
+    if(event.toState === 'void') {
+      this.showMask = false;
+    }
+  }
+
+  onClosePreview() {
+    this.previewImage = false;
+  }
+
+  next(): void {
+    this.currentIndex = this.currentIndex + 1;
+    if(this.currentIndex > this.images.length - 1) {
+      this.currentIndex = 0;
+    }
+    this.currentLightboxImage = this.images[this.currentIndex];
+  }
+
+  prev(): void {
+    this.currentIndex = this.currentIndex - 1;
+    if(this.currentIndex < 0) {
+      this.currentIndex = this.images.length - 1;
+    }
+    this.currentLightboxImage = this.images[this.currentIndex];
+  }
+}
