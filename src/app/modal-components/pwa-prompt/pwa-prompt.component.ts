b9fb7cc8533f6f37d2987793488eb0e0832d5fe1
ANURAG SANGALE <anurag@intelehealth.org>
2023-04-03 09:32:28 +0530

    Bug fixes IDA-765, 763

---
+++ src/app/modal-components/pwa-prompt/pwa-prompt.component.ts
@@ -0,0 +1,26 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_BOTTOM_SHEET_DATA, MatBottomSheetRef } from '@angular/material/bottom-sheet';
+
+@Component({
+  selector: 'app-pwa-prompt',
+  templateUrl: './pwa-prompt.component.html',
+  styleUrls: ['./pwa-prompt.component.scss']
+})
+export class PwaPromptComponent implements OnInit {
+
+  constructor(@Inject(MAT_BOTTOM_SHEET_DATA) public data: { mobileType: 'ios' | 'android', promptEvent?: any },
+  private bottomSheetRef: MatBottomSheetRef<PwaPromptComponent>) { }
+
+  ngOnInit(): void {
+  }
+
+  public installPwa(): void {
+    this.data.promptEvent.prompt();
+    this.close(true);
+  }
+
+  close(val: any) {
+    this.bottomSheetRef.dismiss(val);
+  }
+
+}
