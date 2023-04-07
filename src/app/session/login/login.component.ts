7b812bb81a7bdbc09c3f9e0f503ef4c5f31ed782
ANURAG SANGALE <anurag@intelehealth.org>
2023-04-07 15:14:23 +0530

    Hided remember me functionality

--- src/app/session/login/login.component.ts
+++ src/app/session/login/login.component.ts
@@ -36,7 +36,7 @@ export class LoginComponent implements OnInit {
   get f() { return this.loginForm.controls; }
 
   ngOnInit(): void {
-    this.checkSession();
+    // this.checkSession();
   }
 
   login() {
