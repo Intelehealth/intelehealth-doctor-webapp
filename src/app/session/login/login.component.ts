3612b14c292a62bf9ab9ef419d12604e9c6660e5
ANURAG SANGALE <anurag@intelehealth.org>
2023-05-08 13:36:35 +0530

    Removed otp verification for login

--- src/app/session/login/login.component.ts
+++ src/app/session/login/login.component.ts
@@ -55,16 +55,17 @@ export class LoginComponent implements OnInit {
           if (provider.results.length) {
             localStorage.setItem('provider', JSON.stringify(provider.results[0]));
             localStorage.setItem("doctorName", provider.results[0].person.display);
-            if (res.user.username == 'doctorai' || res.user.username == 'doctor' || res.user.username == 'doctor1' || res.user.username == 'admin' || res.user.systemId == 'admin') {
-              this.loginSuccess();
-            }
-            else if (this.rememberMe) {
-              this.loginSuccess();
-            } else if (provider.results[0].attributes.length) {
-              this.router.navigate(['/session/verification']);
-            } else {
-              this.loginSuccess();
-            }
+            this.loginSuccess();
+            // if (res.user.username == 'doctorai' || res.user.username == 'doctor' || res.user.username == 'doctor1' || res.user.username == 'admin' || res.user.systemId == 'admin') {
+            //   this.loginSuccess();
+            // }
+            // else if (this.rememberMe) {
+            //   this.loginSuccess();
+            // } else if (provider.results[0].attributes.length) {
+            //   this.router.navigate(['/session/verification']);
+            // } else {
+            //   this.loginSuccess();
+            // }
           } else {
             this.toastr.error("Couldn't find provider.", "Login Failed!");
           }
