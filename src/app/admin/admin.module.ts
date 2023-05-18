ce125e8b350bbda1e7ebd8256a15a97dd77fe78b
ANURAG SANGALE <anurag@intelehealth.org>
2023-05-18 18:50:43 +0530

    Bug fix build

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,7 +6,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-// import { AyuComponent } from './ayu/ayu.component';
+import { AyuComponent } from './ayu/ayu.component';
 import { NgSelectModule } from "@ng-select/ng-select";
 import { FormsModule } from '@angular/forms';
 import { MatPaginatorModule } from '@angular/material/paginator';
@@ -41,7 +41,7 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     AdminComponent,
-    // AyuComponent,
+    AyuComponent,
     SupportComponent
   ],
   imports: [
