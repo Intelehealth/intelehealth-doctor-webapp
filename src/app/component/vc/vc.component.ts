8730d70cba6ffd3afb085ea479d13d6ead02b547
Zeeshan <zeeshan@intelehealth.org>
2023-05-29 11:47:53 +0530

    NAS-181

--- src/app/component/vc/vc.component.ts
+++ src/app/component/vc/vc.component.ts
@@ -247,6 +247,16 @@ export class VcComponent implements OnInit, OnDestroy {
       this.pc = new RTCPeerConnection({
         iceServers: [
           {
+            "username": "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
+            "credential": "tE2DajzSJwnsSbc123",
+            "urls": "turn:global.turn.twilio.com:3478?transport=udp"
+          },
+          {
+            "username": "dc2d2894d5a9023620c467b0e71cfa6a35457e6679785ed6ae9856fe5bdfa269",
+            "credential": "tE2DajzSJwnsSbc123",
+            "urls": "turn:global.turn.twilio.com:3478?transport=tcp"
+          },
+          {
             urls: ["turn:demo.intelehealth.org:3478"],
             username: "ihuser",
             credential: "keepitsecrect",
