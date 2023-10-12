3072aac9d0bda4fced67babc34fa6564c5f75a2d
Zeeshan <zeeshan@intelehealth.org>
2023-10-12 18:06:48 +0530

    AEAT-686

--- src/app/component/visit-summary/vital/vital.component.ts
+++ src/app/component/visit-summary/vital/vital.component.ts
@@ -45,6 +45,22 @@ export class VitalComponent implements OnInit {
     return code;
   }
 
+  getBmiColor(n: any) {
+    let code: string = '#FF0000'; // red
+    if (n < 18.5) {
+      code = '#FFA500';
+    } else if (n >= 18.5 && n <= 22.9) {
+      code = '#008000';
+    } else if (n >= 23 && n <= 24.9) {
+      code = '#FFFF00';
+    } else if (n >= 25 && n <= 29.9) {
+      code = '#F85E5EE6';
+    } else if (n >= 30) {
+      code = '#FF0000'
+    }
+    return code;
+  }
+
   ngOnInit() {
     const visitUuid = this.route.snapshot.paramMap.get("visit_id");
     this.visitService.fetchVisitDetails(visitUuid).subscribe((visits) => {
@@ -111,6 +127,10 @@ export class VitalComponent implements OnInit {
               }
               if (displayObs.includes("sugar random")) {
                 this.answer.sugarLevelRandom = obs.value;
+              }
+              if (this.answer?.weight && this.answer?.heightCm) {
+                this.answer.bmi = this.answer.weight / ((this.answer.heightCm / 100) * (this.answer.heightCm / 100))
+                this.answer.bmiColorCode = this.getBmiColor(this.answer.bmi);
               }
             });
             this.v.push(this.answer);
