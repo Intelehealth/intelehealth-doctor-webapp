70adc7edeef5f8849a8f0e02c858555898cff347
Zeeshan <zeeshan@intelehealth.org>
2024-02-07 17:59:47 +0530

    AEAT-177

--- src/app/component/video-library/project/project.component.ts
+++ src/app/component/video-library/project/project.component.ts
@@ -1,39 +1,128 @@
-import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatSort } from "@angular/material/sort";
+import { MatTableDataSource } from "@angular/material/table";
+import { videoLibrary } from "src/app/services/video-library.service";
+import { ModalsComponent } from "../../ayu/modals/modals.component";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 @Component({
-  selector: 'app-project',
-  templateUrl: './project.component.html',
-  styleUrls: ['./project.component.scss']
+  selector: "app-project",
+  templateUrl: "./project.component.html",
+  styleUrls: ["./project.component.scss"],
 })
 export class ProjectComponent implements OnInit {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
-  displayColumns: any[] = [
-    {
-      title: 'Test1',
-      key: 'id'
-    }
+  displayColumns: string[] = [
+    "id",
+    "name",
+    "packageId",
+    "lastUpdate",
+    "action",
+    "manage-video",
   ];
   dataSource: any;
-  constructor() { }
+
+  constructor(
+    private videoLibararySvc: videoLibrary,
+    private dialog: MatDialog,
+    private snackbar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
-    this.dataSource = new MatTableDataSource([
-      {
-        "id": 251,
-        "name": "famHist.json",
-        "keyName": "afiekaltest2021",
-        "createdAt": "2022-07-27T13:29:10.000Z",
-        "updatedAt": "2022-07-27T13:29:10.000Z"
-      },
-      {
-        "id": 252,
-        "name": "patHist.json",
-        "keyName": "afiekaltest2021",
-        "createdAt": "2022-07-27T13:29:18.000Z",
-        "updatedAt": "2022-07-27T13:29:18.000Z"
-      },
-    ])
+    this.getAllProjects();
   }
 
+  getAllProjects() {
+    this.videoLibararySvc.getAllProjects().subscribe({
+      next: (res: any) => {
+        this.dataSource = new MatTableDataSource(res.data);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      },
+    });
+  }
+
+  addProject(): void {
+    const dialogRef = this.dialog.open(ModalsComponent, {
+      data: { title: "Add Project" },
+      width: "40%",
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        const payload = {
+          name: result?.Name,
+          packageId: result?.ProjectId,
+        };
+
+        this.videoLibararySvc.creatProject(payload).subscribe({
+          next: () => {
+            this.snackbar.open("project added successfully", null, {
+              duration: 2500,
+            });
+            this.getAllProjects();
+          },
+        });
+      }
+    });
+  }
+
+  updateProject(id) {
+    const dialogRef = this.dialog.open(ModalsComponent, {
+      data: { title: "Update Project" },
+      width: "40%",
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        const payload = {
+          name: result?.Name,
+          packageId: result?.ProjectId,
+        };
+        this.videoLibararySvc.updateProject(payload, id).subscribe({
+          next: () => {
+            this.snackbar.open("Project updated successfully", null, {
+              duration: 2500,
+            });
+            this.getAllProjects();
+          },
+        });
+      }
+    });
+  }
+
+  delete(id): void {
+    const dialogRef = this.dialog.open(ModalsComponent, {
+      data: { title: "Delete Project" },
+      width: "250px",
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.videoLibararySvc.deleteProject(id).subscribe({
+          next: (res) => {
+            if (res) {
+              this.snackbar.open("Project deleted successfully", null, {
+                duration: 2500,
+              });
+              this.getAllProjects();
+            }
+          },
+        });
+      }
+      this.getAllProjects();
+    });
+    this.getAllProjects();
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
