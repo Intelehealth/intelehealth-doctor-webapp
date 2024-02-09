68b53a3fc2f8dd0366feb299847a53d1db136c2c
dhaval-intelehealth <150244248+dhaval-intelehealth@users.noreply.github.com>
2024-02-09 15:10:49 +0530

    Ida4 3391 (#298)
    * feat: ida4-3391 added authentication header
    
    * fix: ida4-3418 fixed extra backet

--- src/app/core/interceptors/jwt.interceptor.ts
+++ src/app/core/interceptors/jwt.interceptor.ts
@@ -1,23 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { ToastrService } from 'ngx-toastr';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-    constructor(private authService: AuthService, private toastr: ToastrService) {}
+  constructor(private authService: AuthService) { }
 
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-      // add authorization header with jwt token if available
-      const token = this.authService.authToken;
-      if (token && request.url.includes(':3030/v2/node/')) {
-        request = request.clone({
-          setHeaders: {
-            Authorization: `${token}`
-          }
-        });
-      }
-      return next.handle(request);
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    /**
+     * add authorization header with jwt token if available
+     */
+    const token = this.authService.authToken;
+    if (token && !request.url?.includes?.('/openmrs/ws/rest/')) {
+      request = request.clone({
+        setHeaders: { Authorization: `Bearer ${token}` }
+      });
     }
+    return next.handle(request);
+  }
 }
