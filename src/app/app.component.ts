5a513fd9c7344f59992e011b4491d45e6de66501
Akshay Salokhe <akshay@intelehealth.org>
2024-05-17 10:11:12 +0530

    fix: added dynamic colors & home page imgaes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewContainerRef } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { AppConfigService } from './services/app-config.service';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,10 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent implements OnInit {
 
-  constructor(public translate: TranslateService, public vcRef: ViewContainerRef) {
+  primaryColor: string = '#2E1E91';
+  secondaryColor: string = '#1B163A';
+
+  constructor(public translate: TranslateService, private appConfigService: AppConfigService) {
     translate.addLangs(['en', 'ru']);
     translate.setDefaultLang('en');
 
@@ -16,6 +20,10 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.primaryColor = this.appConfigService.theme_config.find(obj=>obj.key==='primary_color')?.value;
+    this.secondaryColor = this.appConfigService.theme_config.find(obj=>obj.key==='secondary_color')?.value;
+    document.documentElement.style.setProperty('--color-darkBlue',  this.primaryColor);
+    document.documentElement.style.setProperty('--color-secondary',  this.secondaryColor);
   }
 
 }
