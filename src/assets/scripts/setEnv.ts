7d8a7eb8a8d105faba79908fa643b7e71907ade6
dhaval-intelehealth <150244248+dhaval-intelehealth@users.noreply.github.com>
2024-07-17 14:33:59 +0530

    Development master webapp 707 (#432)
    * feat: implemented UI for detail ticket
    
    * fix: pagerdutyURL duplicate
    
    * fix: remvoved the file
    
    * fix: changed the discription

--- src/assets/scripts/setEnv.ts
+++ src/assets/scripts/setEnv.ts
@@ -79,8 +79,7 @@ const environmentFileContent = `
     showCaptcha: ${showCaptcha},
     recordsPerPage: 1000,
     abhaEnabled: ${process.env.ABHA_ENABLED},
-    configPublicURL: '${process.env.CONFIG_PUBLIC_URL}',
-    pagerdutyURL: '${process.env.PAGER_DUTY_URL}'
+    configPublicURL: '${process.env.CONFIG_PUBLIC_URL}'
   };
 `;
 
