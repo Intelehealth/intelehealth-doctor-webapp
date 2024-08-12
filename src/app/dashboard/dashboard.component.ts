b64bd55111b616a62f22882baded2ab88c1a0675
dhaval-intelehealth <dhaval@intelehealth.org>
2024-08-12 14:49:23 +0530

    feat: added the notification for reschedule and followup

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -16,6 +16,7 @@ import { getCacheData, checkIfDateOldThanOneDay } from '../utils/utility-functio
 import { doctorDetails, languages, visitTypes } from 'src/config/constant';
 import { ApiResponseModel, AppointmentModel, CustomEncounterModel, CustomObsModel, CustomVisitModel, PatientVisitSummaryConfigModel, ProviderAttributeModel, RescheduleAppointmentModalResponseModel } from '../model/model';
 import { AppConfigService } from '../services/app-config.service';
+import { MindmapService } from '../services/mindmap.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -94,6 +95,7 @@ export class DashboardComponent implements OnInit {
     private coreService: CoreService,
     private toastr: ToastrService,
     private translateService: TranslateService,
+    private mindmapService: MindmapService,
     private appConfigService: AppConfigService) { 
       Object.keys(this.appConfigService.patient_registration).forEach(obj=>{
         this.patientRegFields.push(...this.appConfigService.patient_registration[obj].filter(e=>e.is_enabled).map(e=>e.name));
@@ -434,6 +436,7 @@ export class DashboardComponent implements OnInit {
               this.appointmentService.rescheduleAppointment(appointment).subscribe((res: ApiResponseModel) => {
                 const message = res.message;
                 if (res.status) {
+                  this.mindmapService.notifyHwForRescheduleAppointment(appointment)
                   this.getAppointments();
                   this.toastr.success("The appointment has been rescheduled successfully!", 'Rescheduling successful!');
                 } else {
