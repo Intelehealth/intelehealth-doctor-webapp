7166adb9205ad1ec2c77911f8b97dc3ba4c46707
Vishal Majumdar <vishal.majumdar958@gmail.com>
2026-01-20 12:58:54 +0530

    feat: enhance reminder messages for allergies and current medications

--- projects/aiddx-library/src/lib/aillmtx-medication/aillmtx-medication.component.ts
+++ projects/aiddx-library/src/lib/aillmtx-medication/aillmtx-medication.component.ts
@@ -65,12 +65,12 @@ export class AillmtxMedicationComponent implements OnInit, OnChanges {
     if (!hasAllergies && !hasMedications) {
       this.reminderMessages.push('<strong>Allergy</strong> and <strong>Current Medication status unknown</strong> — confirmation required before prescribing.');
     } else if (hasAllergies && hasMedications) {
-      this.reminderMessages.push(`Patient is currently taking <strong>${this.patientCurrentMedications}</strong>, and Patient is allergic to <strong>${this.patientAllergies}</strong>. Please prescribe accordingly.`);
+      this.reminderMessages.push(`Patient is currently taking <strong>${this.patientCurrentMedications}</strong>, and Patient reports the following allergies to <strong>${this.patientAllergies}</strong>. Please prescribe accordingly.`);
     } else if (!hasAllergies && hasMedications) {
       this.reminderMessages.push('<strong>Allergy status unknown</strong> — confirmation required before prescribing.');
       this.reminderMessages.push(`Patient is currently taking <strong>${this.patientCurrentMedications}</strong>. Please prescribe accordingly.`);
     } else if (hasAllergies && !hasMedications) {
-      this.reminderMessages.push(`Patient is Allergic to <strong>${this.patientAllergies}</strong>. Please prescribe accordingly.`);
+      this.reminderMessages.push(`Patient reports the following allergies to <strong>${this.patientAllergies}</strong>. Please prescribe accordingly.`);
     }
   }
 
